30217f235aa8db63bef9c665839dfc5c9c61e787
David W.H. Swenson <dwhs@hyperblazer.net>
2014-09-14 17:31:39 +0200

    Oops; forgot to add hull_volume in previous commit.

---
+++ code/python/PySRMSTIS/src/hull_volume.py
@@ -0,0 +1,47 @@
+"""
+@author David W.H. Swenson
+"""
+
+from hull import Hull
+from volume import Volume
+
+class HullVolume(Volume):
+    '''Defines a Volume based on a Hull object.
+
+    Parameters
+    ----------
+    hull : Hull
+        a hull object 
+    orderparameter : orderparameter
+        object that takes a system and return a point in the space within
+        which the hull is defined
+    '''
+
+    # TODO: if we find ourselves looking for both Voronoi cells and Delaunay
+    # simplices, one can be sped up by first doing the other
+
+    def __init__(self, hull, orderparameter):
+        super(HullVolume, self).__init__()
+        self.hull = hull
+        self.orderparameter = orderparameter
+
+    def cell(self, snapshot):
+        return self.hull.find_simplex(orderparameter(snapshot))
+
+    def __call__(self, snapshot):
+        '''
+        Returns `True` is the snapshot is within the hull
+
+        Parameters
+        ----------
+        snapshot : Snapshot
+            snapshot to be tested
+
+        Returns
+        -------
+        bool
+            return `True` if snapshot is within the hull
+        '''
+        # with enough simplices, a lookup table is_in_volume[simplex] would
+        # be faster here, but this'll do the job
+        return (self.cell(snapshot) in self.hull.hull_volume_simplices())
