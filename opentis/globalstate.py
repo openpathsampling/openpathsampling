3865e48576a617f1d63dc55aca815673790b3aa0
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2014-11-03 18:36:38 -0500

    Added GlobalState with showtree support

--- opentis/globalstate.py
+++ opentis/globalstate.py
@@ -6,16 +6,16 @@ class GlobalState(dict):
     Notes
     =====
     I would suggest to add the timestamp to the GlobalState object and store it separately and not include
-    the timestamp
+    the timestamp in the samples since these might be saved before a timestamp is set and thus we have
+    mutable objects the might screw up the storage
     """
 
-    now = None       # globsl current state
+    now = None       # global current state
     time = 0         # global current time
 
-    def __init__(self, size, ensembles, time = None):
+    def __init__(self, ensembles, time=None):
         super(GlobalState, self).__init__()
-        self.size = size
-        self.ensembles = ensembles
+        self._ensembles = ensembles
         self.old = None
         self.samples = []
         self.clear()
@@ -26,14 +26,68 @@ class GlobalState(dict):
             GlobalState.time = time
 
         self.time = time
-        for ensemble in ensembles:
-            self[ensemble] = None
+        for ensemble in self._ensembles:
+            dict.__setitem__(self, ensemble, None)
 
         GlobalState.now = self
 
+    def __setitem__(self, key, value):
+        if key in self:
+            if self[key] is None:
+                dict.__setitem__(self, key, value)
+            else:
+                print 'Cannot set a trajectory for this Ensemble. Already set. ' \
+                      'Trajectories can only be set once. Otherwise moves are necessary!'
+
+        else:
+            print 'Cannot change size of GlobalState. Ensemble are fixed at creation!'
+
+    def __getitem__(self, item):
+        if type(item) is int:
+            return dict.__getitem__(self, self.ensembles[item])
+        else:
+            return dict.__getitem__(self, item)
+
+    @property
+    def size(self):
+        """
+        Returns the number of unique ensembles
+
+        Returns
+        =======
+        int
+            The number of ensembles / trajectories in the GlobalState
+        """
+        return len(self._ensembles)
+
+    @property
+    def ensembles(self):
+        """
+        Return an orderes list of ensembles in the GlobalState
+
+        Returns
+        =======
+        list of Ensemble()
+            The initial list of ensembles in the given order in the GlobalState
+        """
+        return self._ensembles
+
+    @property
+    def trajectories(self):
+        """
+        Return an ordered list of trajectories in the GlobalState()
+
+        Returns
+        =======
+        list of Trajectory()
+            The list of trajectories
+        """
+        return [ self[ensemble] for ensemble in self._ensembles]
+
     def move(self, samples):
         """
-        Returns a new GlobalState object that takes the current instance and applies the sample in the given order as updates.
+        Returns a new GlobalState object that takes the current instance and applies the samples in the given order as updates.
+        The samples will get the actual timestamp for later analysis.
 
         samples : list of Sample()
             The list of `Sample` objects used to update the current GlobalState
@@ -43,15 +97,23 @@ class GlobalState(dict):
         GlobalState()
 
         """
-        globalstate = GlobalState(self.size, self.ensembles)
+        globalstate = GlobalState(self.ensembles)
         globalstate.time = self.time + 1
         globalstate.old = self
-        globalstate.samples = samples
+        self.samples = samples
         for sample in samples:
             sample.time = self.time
 
-            globalstate.ensemble_set[sample.ensemble] = sample.trajectory
+            dict.__setitem__(globalstate, sample.ensemble, sample.trajectory)
 
         GlobalState.now = globalstate
-
         return globalstate
+
+    def save_samples(self, storage):
+        """
+        Save all samples in the current GlobalState object. This should be called after a move has generated a new object since then
+        all samples will get a timestamp that is associated with this
+        :param storage:
+        :return:
+        """
+        map(storage.sample.save, self.samples)
--- opentis/showtree.py
+++ opentis/showtree.py
@@ -127,46 +127,47 @@ if __name__ == '__main__':
 
     for o_idx in range(1, storage.sample.count() + 1):
         sample = storage.sample.load(o_idx)
-        length = len(sample.details.final)
+        if hasattr(sample.details, 'start_point'):
+            length = len(sample.details.final)
 
-        old_traj = sample.details.start_point.trajectory
-        old_index = sample.details.start_point.index
-        old_conf = old_traj[old_index].configuration
+            old_traj = sample.details.start_point.trajectory
+            old_index = sample.details.start_point.index
+            old_conf = old_traj[old_index].configuration
 
-        new_traj = sample.details.final_point.trajectory
-        new_index = sample.details.final_point.index
-        new_conf = new_traj[new_index].configuration
+            new_traj = sample.details.final_point.trajectory
+            new_index = sample.details.final_point.index
+            new_conf = new_traj[new_index].configuration
 
-        if sample.trajectory is new_traj or rejected:
-            accepted = sample.trajectory is new_traj
-            t_count += 0.8
-            if not old_conf in p_x:
-                for pos, snapshot in enumerate(old_traj):
-                    conf = snapshot.configuration
-                    p_x[conf] = pos
-                    p_y[conf] = t_count
-
-                    pos_x = p_x[conf]
-                    pos_y = p_y[conf]
-
+            if sample.trajectory is new_traj or rejected:
+                accepted = sample.trajectory is new_traj
                 t_count += 0.8
+                if not old_conf in p_x:
+                    for pos, snapshot in enumerate(old_traj):
+                        conf = snapshot.configuration
+                        p_x[conf] = pos
+                        p_y[conf] = t_count
 
-                min_x = min(min_x, 0)
-                max_x = max(max_x, len(old_traj) - 1)
+                        pos_x = p_x[conf]
+                        pos_y = p_y[conf]
 
-            shift = p_x[old_conf] - new_index
+                    t_count += 0.8
 
-            min_x = min(min_x, shift)
-            max_x = max(max_x, shift + len(new_traj) - 1)
+                    min_x = min(min_x, 0)
+                    max_x = max(max_x, len(old_traj) - 1)
 
-            for pos, snapshot in enumerate(new_traj):
-                conf = snapshot.configuration
-                if not conf in p_y:
-                    p_y[conf] = t_count
-                    p_x[conf] = shift + pos
+                shift = p_x[old_conf] - new_index
 
-                    pos_x = p_x[conf]
-                    pos_y = p_y[conf]
+                min_x = min(min_x, shift)
+                max_x = max(max_x, shift + len(new_traj) - 1)
+
+                for pos, snapshot in enumerate(new_traj):
+                    conf = snapshot.configuration
+                    if not conf in p_y:
+                        p_y[conf] = t_count
+                        p_x[conf] = shift + pos
+
+                        pos_x = p_x[conf]
+                        pos_y = p_y[conf]
 
     svg_document = svgwrite.Drawing(
         filename = "tree.svg"
@@ -194,96 +195,117 @@ if __name__ == '__main__':
 
     for o_idx in range(1, storage.sample.count() + 1):
         sample = storage.sample.load(o_idx)
-        length = len(sample.details.final)
+        if hasattr(sample.details, 'start_point'):
+            length = len(sample.details.final)
 
-        old_traj = sample.details.start_point.trajectory
-        old_index = sample.details.start_point.index
-        old_conf = old_traj[old_index].configuration
+            old_traj = sample.details.start_point.trajectory
+            old_index = sample.details.start_point.index
+            old_conf = old_traj[old_index].configuration
 
-        new_traj = sample.details.final_point.trajectory
-        new_index = sample.details.final_point.index
-        new_conf = new_traj[new_index].configuration
+            new_traj = sample.details.final_point.trajectory
+            new_index = sample.details.final_point.index
+            new_conf = new_traj[new_index].configuration
 
-        if sample.trajectory is new_traj or rejected:
-            accepted = sample.trajectory is new_traj
-            t_count += 0.8
-            if not old_conf in p_x:
-                for pos, snapshot in enumerate(old_traj):
-                    conf = snapshot.configuration
-                    p_x[conf] = pos
-                    p_y[conf] = t_count
-
-                    pos_x = p_x[conf]
-                    pos_y = p_y[conf]
-
-                    if show_psi:
-                        block(pos_x, pos_y, "black", str(int((degrees * psi(conf))) % 360 ))
-                    else:
-                        block(pos_x, pos_y, "black", conf.idx[storage])
-
+            if sample.trajectory is new_traj or rejected:
+                accepted = sample.trajectory is new_traj
                 t_count += 0.8
+                if not old_conf in p_x:
+                    for pos, snapshot in enumerate(old_traj):
+                        conf = snapshot.configuration
+                        p_x[conf] = pos
+                        p_y[conf] = t_count
 
-            shift = p_x[old_conf] - new_index
+                        pos_x = p_x[conf]
+                        pos_y = p_y[conf]
 
-            fontcolor = "black"
+                        if show_psi:
+                            block(pos_x, pos_y, "black", str(int((degrees * psi(conf))) % 360 ))
+                        else:
+                            block(pos_x, pos_y, "black", conf.idx[storage])
 
-            if sample.details.mover.name == "BackwardShootMover":
-                color = "green"
-                if not accepted:
+                    t_count += 0.8
+
+                shift = p_x[old_conf] - new_index
+
+                fontcolor = "black"
+
+                if sample.details.mover.name == "BackwardShootMover":
+                    color = "green"
+                    if not accepted:
+                        color = lightcolor
+                        fontcolor = lightcolor
+                    svg_document.add(svg_document.line(
+                        start = (start_x + (shift + new_index) * scale_x + 0,start_y + (p_y[old_conf] + 0.05) * scale_y),
+                        end = (start_x  + (shift + new_index) * scale_x + 0,start_y + (t_count - 0.05) * scale_y),
+                        stroke_width = 0.05 * scale_x,
+                        stroke = color,
+                    ))
+                    svg_document.add(svg_document.text(
+                                            text = str(new_traj.idx[storage]) + 'b',
+                                            insert = (start_x + (shift + 0 - 0.2) * scale_x,start_y + (t_count + 0.05) * scale_y),
+                                            text_anchor = 'end',
+                                            font_size = 0.4*scale_y,
+                                            alignment_baseline = 'middle',
+                                            font_family = 'Futura',
+                                            fill = fontcolor
+                                            ))
+                else:
+                    color = "red"
+                    if not accepted:
+                        color = lightcolor
+                        fontcolor = lightcolor
+
+                    svg_document.add(svg_document.line(
+                        start = (start_x + (shift + new_index + 1) * scale_x,start_y + 0.5 + p_y[old_conf] * scale_y),
+                        end = (start_x  + (shift + new_index + 1) * scale_x,start_y - 0.5 + t_count * scale_y),
+                        stroke_width = 0.05 * scale_x,
+                        stroke = color,
+                    ))
+                    svg_document.add(svg_document.text(
+                                            text = str(new_traj.idx[storage]) + 'f',
+                                            insert = (start_x + (shift + len(new_traj) + 0.2) * scale_x,start_y + (t_count + 0.05) * scale_y),
+                                            text_anchor = 'start',
+                                            font_size = 0.4*scale_y,
+                                            alignment_baseline = 'middle',
+                                            font_family = 'Futura',
+                                            fill = fontcolor
+                                            ))
+
+                if not  accepted:
                     color = lightcolor
-                    fontcolor = lightcolor
-                svg_document.add(svg_document.line(
-                    start = (start_x + (shift + new_index) * scale_x + 0,start_y + (p_y[old_conf] + 0.05) * scale_y),
-                    end = (start_x  + (shift + new_index) * scale_x + 0,start_y + (t_count - 0.05) * scale_y),
-                    stroke_width = 0.05 * scale_x,
-                    stroke = color,
-                ))
-                svg_document.add(svg_document.text(
-                                        text = str(new_traj.idx[storage]) + 'b',
-                                        insert = (start_x + (shift + 0 - 0.2) * scale_x,start_y + (t_count + 0.05) * scale_y),
-                                        text_anchor = 'end',
-                                        font_size = 0.4*scale_y,
-                                        alignment_baseline = 'middle',
-                                        font_family = 'Futura',
-                                        fill = fontcolor
-                                        ))
-            else:
-                color = "red"
-                if not accepted:
-                    color = lightcolor
-                    fontcolor = lightcolor
 
-                svg_document.add(svg_document.line(
-                    start = (start_x + (shift + new_index + 1) * scale_x,start_y + 0.5 + p_y[old_conf] * scale_y),
-                    end = (start_x  + (shift + new_index + 1) * scale_x,start_y - 0.5 + t_count * scale_y),
-                    stroke_width = 0.05 * scale_x,
-                    stroke = color,
-                ))
-                svg_document.add(svg_document.text(
-                                        text = str(new_traj.idx[storage]) + 'f',
-                                        insert = (start_x + (shift + len(new_traj) + 0.2) * scale_x,start_y + (t_count + 0.05) * scale_y),
-                                        text_anchor = 'start',
-                                        font_size = 0.4*scale_y,
-                                        alignment_baseline = 'middle',
-                                        font_family = 'Futura',
-                                        fill = fontcolor
-                                        ))
+                for pos, snapshot in enumerate(new_traj):
+                    conf = snapshot.configuration
+                    if not conf in p_y:
+                        p_y[conf] = t_count
+                        p_x[conf] = shift + pos
 
-            if not  accepted:
-                color = lightcolor
+                        pos_x = p_x[conf]
+                        pos_y = p_y[conf]
 
-            for pos, snapshot in enumerate(new_traj):
+                        if show_psi:
+                            block(pos_x, pos_y, color, str(int((degrees * psi(conf))) % 360 ))
+                        else:
+                            block(pos_x, pos_y, color, conf.idx[storage])
+
+        else:
+            accepted = sample.trajectory is new_traj
+            t_count += 2 * 0.8
+            # Show full trajectory once
+            for pos, snapshot in enumerate(sample.trajectory):
                 conf = snapshot.configuration
-                if not conf in p_y:
-                    p_y[conf] = t_count
-                    p_x[conf] = shift + pos
+                p_x[conf] = pos
+                p_y[conf] = t_count
 
-                    pos_x = p_x[conf]
-                    pos_y = p_y[conf]
+                pos_x = p_x[conf]
+                pos_y = p_y[conf]
 
-                    if show_psi:
-                        block(pos_x, pos_y, color, str(int((degrees * psi(conf))) % 360 ))
-                    else:
-                        block(pos_x, pos_y, color, conf.idx[storage])
+                if show_psi:
+                    block(pos_x, pos_y, "black", str(int((degrees * psi(conf))) % 360 ))
+                else:
+                    block(pos_x, pos_y, "black", conf.idx[storage])
+
+        shift = p_x[old_conf] - new_index
+
 
     svg_document.save()
