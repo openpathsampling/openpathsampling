c85d1290549f643f5a60106060c7a04ad4df3ad4
David W.H. Swenson <dwhs@hyperblazer.net>
2014-11-05 11:06:23 +0100

    nosetests can be run from main pkg dir

--- opentis/tests/__init__.py
+++ opentis/tests/__init__.py
@@ -3,20 +3,23 @@ from opentis.trajectory import Trajectory
 from opentis.Simulator import Simulator
 import mdtraj as md
 
+from test_helpers import data_filename
+
 def setup_package():
     # this should generate the trajectory.nc file which we'll use for
     # everything else
-    mdtrajectory = md.load("test_data/ala_small_traj.pdb")
+    mdtrajectory = md.load(data_filename("ala_small_traj.pdb"))
     simulator = Simulator()
     simulator.storage = Storage(
-        topology_file="test_data/ala_small_traj.pdb",
-        filename="test_data/ala_small_traj.nc",
+        topology_file=data_filename("ala_small_traj.pdb"),
+        filename=data_filename("ala_small_traj.nc"),
         mode='w'
     )
     simulator.storage.simulator = simulator
     Trajectory.storage = simulator.storage
     mytraj = Trajectory.from_mdtraj(mdtrajectory)
     simulator.storage.trajectory.save(mytraj)
+
 
 def teardown_package():
     # this should delete the trajectory.nc file
--- opentis/tests/testorderparameter.py
+++ opentis/tests/testorderparameter.py
@@ -6,12 +6,10 @@ import os
 import sys
 from nose.tools import assert_equal, assert_not_equal, raises
 from nose.plugins.skip import Skip, SkipTest
-from test_helpers import AtomCounter, SimulationDuckPunch
+from test_helpers import AtomCounter, SimulationDuckPunch, data_filename
 
 from nose.tools import assert_equal
 import mdtraj as md
-
-from duckpunching import SimulationDuckPunch
 
 import opentis.trajectory as trajectory
 from opentis.storage import Storage
@@ -24,11 +22,11 @@ class testOP_Function(object):
         # setUp is just reading in some alanine dipeptide frames: this is an
         # ugly hack
         self.storage = Storage(topology_file=None,
-                               filename="../data/trajectory.nc",
+                               filename=data_filename("ala_small_traj.nc"),
                                mode="a"
                               )
 
-        topol = md.load("../data/Alanine_solvated.pdb").top.to_openmm()
+        topol = md.load(data_filename("ala_small_traj.pdb")).top.to_openmm()
         self.simulation = SimulationDuckPunch(topol, None)
 
         trajectory.Trajectory.simulator = self
@@ -44,9 +42,9 @@ class testOP_Function(object):
         dihedral_op = op.OP_Function("psi", md.compute_dihedrals,
                                     trajdatafmt="mdtraj",
                                     indices=[psi_atoms])
-        mdtraj_version = self.storage.trajectory.load(1).md()
+        mdtraj_version = self.storage.trajectory.load(0).md()
         md_dihed = md.compute_dihedrals(mdtraj_version, indices=[psi_atoms])
-        traj = self.storage.trajectory.load(1)
+        traj = self.storage.trajectory.load(0)
 
         my_dihed =  dihedral_op( traj )
         for (truth, beauty) in zip(md_dihed, my_dihed):
