96eec66f41bf051a89d259e63420236e7be1bd11
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2014-11-10 00:11:26 -0500

    added new example for showtree

---
+++ opentis/showtree_new.py
@@ -0,0 +1,105 @@
+import svgwrite
+import sys
+import argparse
+import os
+from storage import Storage
+from visualize import PathTreeBuilder
+
+if __name__ == '__main__':
+
+
+    parser = argparse.ArgumentParser(description='Analyze a file.')
+    parser.add_argument('--rejected', dest='rejected', action='store_const',
+                   const=True, default=False,
+                   help='show also rejected paths')
+    parser.add_argument('file', metavar='file.nc', help='an integer for the accumulator')
+    parser.add_argument('--show', metavar='orderparameter/snapshot/configuration/momentum', default='',
+                   help='show an orderparameter or an id of the snapshot/configuration/momentum')
+
+    parser.add_argument('--pdf', dest='pdf', action='store_const',
+                   const=True, default=False,
+                   help='create a pdf instead of a svg. Uses wkhtmltopdf to make apdf using webkit and qt')
+
+    parser.add_argument('--html', dest='html', action='store_const',
+                   const=True, default=False,
+                   help='create a svg embedded in a html')
+
+
+    parser.add_argument('--state', nargs=2, action='append',
+                        help='add a background coloring to s snapshot using the given orderparameter.',
+                        metavar=('orderparameter', 'color'))
+
+    parser.add_argument('--show-degree', dest='in_degree', action='store_const',
+                   const=True, default=False,
+                   help='shows orderparameters in degree and not plain')
+
+    args = parser.parse_args()
+
+    rejected = args.rejected
+    file = args.file
+
+    degrees = 180/3.14159 # psi reports in radians; I think in degrees
+
+    if not os.path.isfile(file):
+        print file, 'does not exist ! ENDING!'
+        exit()
+
+    storage = Storage(
+         filename = file,
+         mode = 'a'
+         )
+
+
+    if args.show == 'snapshot':
+        def show_id(s):
+            return s.idx[storage]
+
+        show_op = show_id
+    elif args.show == 'configuration':
+        def show_id(s):
+            return s.configuration.idx[storage]
+
+        show_op = show_id
+    elif args.show == 'momentum':
+        def show_id(s):
+            return s.momentum.idx[storage]
+
+        show_op = show_id
+    elif args.show == '':
+        def show_id(s):
+            return ''
+
+        show_op = show_id
+    else:
+        show_op = storage.cv.load(args.show)
+
+    if args.in_degree:
+        inner = show_op
+        def func(s):
+            return degrees * inner(s)
+
+        show_op = func
+
+    tree = PathTreeBuilder(storage, op=show_op, states=args.state)
+
+    samples = storage.sample.by_ensemble(storage.ensemble.load(4))
+
+    print 'Samples : ', len(samples)
+
+    tree.from_samples(samples)
+
+    if args.pdf:
+
+        tree.renderer.save_pdf()
+
+        bashCommand = "open tree.pdf"
+        os.system(bashCommand)
+    elif args.html:
+        tree.renderer.write_html()
+
+        bashCommand = "open -a Safari tree.html"
+        os.system(bashCommand)
+
+    else:
+        bashCommand = "open -a Safari tree.svg"
+    #    os.system(bashCommand)
