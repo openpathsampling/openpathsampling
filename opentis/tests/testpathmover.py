f8b7e907b173af1cf0c18624a1b562ef97cf9b30
David W.H. Swenson <dwhs@hyperblazer.net>
2014-12-16 16:31:56 +0100

    Tests for make_list_of_pairs

--- opentis/tests/testpathmover.py
+++ opentis/tests/testpathmover.py
@@ -8,27 +8,34 @@ import numpy as np
 from nose.tools import (assert_equal, assert_not_equal, assert_items_equal,
                         assert_almost_equal, raises)
 from nose.plugins.skip import Skip, SkipTest
+from test_helpers import assert_equal_array_array, assert_not_equal_array_array
 
 from opentis.pathmover import *
 
 class testMakeListOfPairs(object):
     def setup(self):
+        self.correct = [ [0, 1], [2, 3], [4, 5] ]
         pass
 
+    @raises(TypeError)
     def test_not_iterable_type_error(self):
-        raise SkipTest
+        result = make_list_of_pairs(1)
     
-    def test_list_of_list_pairs(object):
-        raise SkipTest
+    def test_list_of_list_pairs(self):
+        result = make_list_of_pairs([[0, 1], [2, 3], [4, 5]])
+        assert_equal_array_array(result, self.correct)
 
-    def test_list_of_list_notpairs(object):
-        raise SkipTest
+    @raises(AssertionError)
+    def test_list_of_list_notpairs(self):
+        result = make_list_of_pairs([[0], [1], [2]])
 
-    def test_list_not_even(object):
-        raise SkipTest
+    @raises(AssertionError)
+    def test_list_not_even(self):
+        result = make_list_of_pairs([0, 1, 2, 3, 4])
 
-    def test_list_even(object):
-        raise SkipTest
+    def test_list_even(self):
+        result = make_list_of_pairs([0, 1, 2, 3, 4, 5])
+        assert_equal_array_array(result, self.correct)
 
 class testPathMover(object):
     def test_legal_sample_set(self):
