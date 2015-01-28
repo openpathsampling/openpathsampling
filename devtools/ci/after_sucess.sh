b028fd92731f658a7fb97e6345e75fe200da170d
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2015-01-27 22:07:24 -0500

    replaced -u with --username

--- devtools/ci/after_sucess.sh
+++ devtools/ci/after_sucess.sh
@@ -12,8 +12,8 @@ echo $TRAVIS_PULL_REQUEST $TRAVIS_BRANCH
 #fi
 
 if [[ "2.7" =~ "$python" ]]; then
-    conda install --yes binstar
-    binstar -t $BINSTAR_TOKEN  upload --force -u omnia -p openpathsampling-dev $HOME/miniconda/conda-bld/linux-64/opentis-dev-*
+    conda install --yes binstar jinja2
+    binstar -t $BINSTAR_TOKEN  upload --force --username omnia -p openpathsampling-dev $HOME/miniconda/conda-bld/linux-64/opentis-dev-*
 fi
 
 echo "Not yet uploading to s3."
