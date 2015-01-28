04cbbd4bd081b05e3bfe5b677d2e148da6fe551a
David W.H. Swenson <dwhs@hyperblazer.net>
2015-01-28 13:04:36 +0100

    Tests for FirstFrameSelector pass
    Next, do the same for FinalFrameSelector.

--- opentis/tests/testshooting.py
+++ opentis/tests/testshooting.py
@@ -4,16 +4,25 @@ from nose.tools import (assert_equal, assert_not_equal, assert_items_equal,
 from nose.plugins.skip import Skip, SkipTest
 from test_helpers import (assert_equal_array_array, 
                           assert_not_equal_array_array,
-                          make_1d_traj
+                          make_1d_traj,
+                          CalvinDynamics
                          )
 from opentis.shooting import *
-
-from opentis.pathmover import PathMover, OneWayShootingMover
+from opentis.pathmover import PathMover, ForwardShootMover, BackwardShootMover
+from opentis.ensemble import LengthEnsemble
+from opentis.sample import Sample, SampleSet
 
 class SelectorTest(object):
     def setup(self):
-        self.mytraj = make_1d_traj([-0.5, 0.1, 0.3, 0.5])
-        #self.shooter = OneWayShootingMover(FirstFrameSelector())
+        self.mytraj = make_1d_traj([-0.5, 0.1, 0.2, 0.3, 0.5])
+        self.dyn = CalvinDynamics([-0.5, -0.4, -0.3, -0.2, -0.1,
+                                   0.5, 0.4, 0.3, 0.2, 0.1])
+        PathMover.engine = self.dyn
+        self.dyn.initialized = True
+        self.ens = LengthEnsemble(5)
+        self.gs = SampleSet(Sample(
+            replica=0, trajectory=self.mytraj, ensemble=self.ens
+        ))
 
 
 class testFirstFrameSelector(SelectorTest):
@@ -29,8 +38,13 @@ class testFirstFrameSelector(SelectorTest):
         assert_equal(snap.coordinates[0][0], -0.5)
 
     def test_shooting_move(self):
-        # this just tests whether the shooting move runs, not correctness
-        raise SkipTest
+        self.shooter = ForwardShootMover(FirstFrameSelector(), replicas=[0])
+        samples = self.shooter.move(self.gs)
+        assert_equal(len(samples), 1)
+        assert_equal(samples[0].details.accepted, True)
+        assert_items_equal([-0.5, -0.4, -0.3, -0.2, -0.1],
+                           [s.coordinates[0][0] for s in samples[0].trajectory]
+                          )
 
 class testFinalFrameSelector(SelectorTest):
     def test_pick(self):
