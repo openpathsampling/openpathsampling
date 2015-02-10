c35573cee32ac774bf130363a89dcfa74e617cf5
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2015-02-10 18:34:06 -0500

    removed unused imports

--- openpathsampling/todict.py
+++ openpathsampling/todict.py
@@ -1,10 +1,7 @@
 import base64
 import json
-import mdtraj as md
 import numpy as np
-import pandas as pd
 from simtk import unit as units
-import simtk.openmm
 import yaml
 import openpathsampling as paths
 
@@ -224,41 +221,41 @@ def restores_as_stub_object(super_class):
 
     return super_class
 
-def restores_as_stub_object_old(super_class):
-    """
-    A class decorator that marks a class to be storable in the storage using a LoadedObject class.
-    This object will have the same class name, the same dict, but none of the functions and will not have
-    been initialized. If you want real objects use @creatable
-    :param super_class: The class to be decorated
-    :return: The decorated class
-    """
-    class_list[super_class.__name__] = super_class
-
-    super_class.dictable = True
-    if not hasattr(super_class, '_excluded_attr'):
-        super_class._excluded_attr = []
-
-    if not hasattr(super_class, 'to_dict'):
-        def _to_dict(self):
-            excluded_keys = ['idx']
-            return {key: value for key, value in self.__dict__.iteritems() if key not in excluded_keys and key not in self._excluded_attr and not key.startswith('_')}
-
-        super_class.to_dict = _to_dict
-
-    if not hasattr(super_class, 'from_dict'):
-        def _from_dict(cls, my_dict = None):
-            if my_dict is None:
-                my_dict={}
-
-            obj = LoadedObject()
-
-            for key, value in my_dict.iteritems():
-                setattr(obj, key, value)
-
-            setattr(obj, '_cls', cls.__name__)
-
-            return obj
-
-        super_class.from_dict = classmethod(_from_dict)
-
-    return super_class
+# def restores_as_stub_object_old(super_class):
+#     """
+#     A class decorator that marks a class to be storable in the storage using a LoadedObject class.
+#     This object will have the same class name, the same dict, but none of the functions and will not have
+#     been initialized. If you want real objects use @creatable
+#     :param super_class: The class to be decorated
+#     :return: The decorated class
+#     """
+#     class_list[super_class.__name__] = super_class
+#
+#     super_class.dictable = True
+#     if not hasattr(super_class, '_excluded_attr'):
+#         super_class._excluded_attr = []
+#
+#     if not hasattr(super_class, 'to_dict'):
+#         def _to_dict(self):
+#             excluded_keys = ['idx']
+#             return {key: value for key, value in self.__dict__.iteritems() if key not in excluded_keys and key not in self._excluded_attr and not key.startswith('_')}
+#
+#         super_class.to_dict = _to_dict
+#
+#     if not hasattr(super_class, 'from_dict'):
+#         def _from_dict(cls, my_dict = None):
+#             if my_dict is None:
+#                 my_dict={}
+#
+#             obj = LoadedObject()
+#
+#             for key, value in my_dict.iteritems():
+#                 setattr(obj, key, value)
+#
+#             setattr(obj, '_cls', cls.__name__)
+#
+#             return obj
+#
+#         super_class.from_dict = classmethod(_from_dict)
+#
+#     return super_class
