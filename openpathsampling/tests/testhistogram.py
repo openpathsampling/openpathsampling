8f751371243c7197c8a67a46bfb199edcf37bf73
David W.H. Swenson <dwhs@hyperblazer.net>
2015-02-10 12:28:31 +0100

    Add skeleton of testhistogram

---
+++ openpathsampling/tests/testhistogram.py
@@ -0,0 +1,43 @@
+from nose.tools import assert_equal, assert_not_equal, raises
+from nose.plugins.skip import SkipTest
+
+import logging
+
+from openpathsampling.analysis import Histogram
+
+class testHistogram(object):
+    def setup(self):
+        pass
+
+    def test_initialization_with_nbins(self):
+        raise SkipTest
+
+    def test_initialization_with_nbins_range(self):
+        raise SkipTest
+
+    def test_initialization_with_binwidth(self):
+        raise SkipTest
+
+    def test_initialization_fail(self):
+        raise SkipTest
+
+    def test_build_from_data(self):
+        raise SkipTest
+
+    def test_add_data_to_histogram(self):
+        raise SkipTest
+
+    def test_compare_parameters(self):
+        raise SkipTest
+
+    def test_normalized(self):
+        raise SkipTest
+
+    def test_cumulative(self):
+        raise SkipTest
+
+    def test_reverse_cumulative(self):
+        raise SkipTest
+
+
+
