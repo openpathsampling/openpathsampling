31e230d5309b6e66595ccedff80f174d2948b198
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2015-03-03 21:54:36 +0100

    close file after pdb read

--- openpathsampling/openmm_engine.py
+++ openpathsampling/openmm_engine.py
@@ -268,6 +268,7 @@ class OpenMMEngine(paths.DynamicsEngine):
 
     def generate_next_frame(self):
         print 'run for steps ', self.nsteps_per_frame
+        print self.simulation
         self.simulation.step(self.nsteps_per_frame)
         self._current_snapshot = None
         print 'finished steps'
