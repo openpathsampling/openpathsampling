99b6b07c7c3b5b0bece82b3acb37512aac55b6c2
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2015-03-06 19:27:00 +0100

    fix ipythontests.sh

--- devtools/ci/ipythontests.sh
+++ devtools/ci/ipythontests.sh
@@ -1,16 +1,17 @@
+#!/bin/sh
 # Run ipython notebook tests
 
 cd examples/ipython
 testfail=0
-python ipnbdoctest.py "alanine.ipynb" || testfail=1
-python ipnbdoctest.py "sliced_sequential_ensembles.ipynb" || testfail=1
-python ipnbdoctest.py "toy_dynamics_tis.ipynb" || testfail=1
-python ipnbdoctest.py "toy_storage.ipynb" || testfail=1
-python ipnbdoctest.py "langevin_integrator_check.ipynb" || testfail=1
-python ipnbdoctest.py "sliced_sequential_ensembles.ipynb" || testfail=1
+python ipynbtest.py "alanine.ipynb" || testfail=1
+python ipynbtest.py "sliced_sequential_ensembles.ipynb" || testfail=1
+python ipynbtest.py "toy_dynamics_tis.ipynb" || testfail=1
+python ipynbtest.py "toy_storage.ipynb" || testfail=1
+python ipynbtest.py "langevin_integrator_check.ipynb" || testfail=1
+python ipynbtest.py "sliced_sequential_ensembles.ipynb" || testfail=1
 # needs to run after alanine since it need the trajectory.nc file
-python ipnbdoctest.py "storage_tutorial.ipynb" || testfail=1
-# python ipnbdoctest.py "visualization.ipynb" || testfail=1
+python ipynbtest.py "storage_tutorial.ipynb" || testfail=1
+# python ipynbtest.py "visualization.ipynb" || testfail=1
 cd ../..
 if [ $testfail -eq 1 ]
 then
