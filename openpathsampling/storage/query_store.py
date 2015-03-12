ba22eaeb39c2afb7d59f0c0467722ed92c350b7d
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2015-03-13 00:40:55 +0100

    add query function

--- openpathsampling/storage/query_store.py
+++ openpathsampling/storage/query_store.py
@@ -4,11 +4,38 @@ class QueryStore():
         self.storage = storage
 
 
-    def orderparameter_trajectory(self, orderparameter, ensemble=None, replica=None, step=None, trial=False):
+    def trajectory_orderparameter(self, orderparameter, ensemble=None, replica=None, step=None):
+        """
+        Return list of orderparameters fast for specific sets of samples
+
+        samples can be all samples found in specific or all sampleset and filter
+        these by ensemble and/or replica.
+
+        Parameters
+        ----------
+        orderparameter : paths.Orderparameter()
+            the orderparameter from which the values should be extracted
+        ensemble : paths.Ensemble or None
+            if not None only samples from the specific ensemble are used.
+            For `None` (default) all ensembles are considered
+        replica : int or None
+            if not None only samples from the specific replica ID are used.
+            For `None` (default) all replica IDs are considered.
+        step : int or None
+            if not None only samples from the specific step are used.
+            For `None` (default) all sampleset steps are considered.
+
+        Returns
+        -------
+        list of list of float
+            Returns for each sample a list of floats which represent the
+            orderparameter values of the trajectory of the samples
+        """
 
         storage = self.storage
 
-        ens_idx = ensemble.idx[storage]
+        if ensemble is not None:
+            ens_idx = ensemble.idx[storage]
 
         output = []
 
@@ -19,9 +46,8 @@ class QueryStore():
                 continue
 
             sample_idxs = storage.variables['sampleset_sample_idx'][sset_id].tolist()
-
             ensemble_idxs = storage.variables['sample_ensemble_idx'][sample_idxs].tolist()
-            replica_idxs = storage.variables['sample_replica_idx'][sample_idxs].tolist()
+            replica_idxs = storage.variables['sample_replica'][sample_idxs].tolist()
             traj_idx = storage.variables['sample_trajectory_idx'][sample_idxs].tolist()
 
             for no, sample_idx in enumerate(sample_idxs):
@@ -33,5 +59,57 @@ class QueryStore():
                 snap_idxs = storage.variables['trajectory_snapshot_idx'][traj_idx[no]]
 
                 output.append([ op_dict[idx] for idx in snap_idxs ])
+
+        return output
+
+    def trajectory_length(self, ensemble=None, replica=None, step=None):
+        """
+        Return list of trajectory lengths fast for specific sets of samples
+
+        samples can be all samples found in specific or all sampleset and filter
+        these by ensemble and/or replica.
+
+        Parameters
+        ----------
+        ensemble : paths.Ensemble or None
+            if not None only samples from the specific ensemble are used.
+            For `None` (default) all ensembles are considered
+        replica : int or None
+            if not None only samples from the specific replica ID are used.
+            For `None` (default) all replica IDs are considered.
+        step : int or None
+            if not None only samples from the specific step are used.
+            For `None` (default) all sampleset steps are considered.
+
+        Returns
+        -------
+        list of int
+            Returns for each sample the number of snapshots in it
+        """
+        storage = self.storage
+
+        if ensemble is not None:
+            ens_idx = ensemble.idx[storage]
+
+        output = []
+
+        for sset_id in range(len(storage.sampleset)):
+            if step is not None and sset_id != step:
+                continue
+
+            sample_idxs = storage.variables['sampleset_sample_idx'][sset_id].tolist()
+            ensemble_idxs = storage.variables['sample_ensemble_idx'][sample_idxs].tolist()
+            replica_idxs = storage.variables['sample_replica'][sample_idxs].tolist()
+            traj_idx = storage.variables['sample_trajectory_idx'][sample_idxs].tolist()
+
+            for no, sample_idx in enumerate(sample_idxs):
+                if ensemble is not None and ens_idx != ensemble_idxs[no]:
+                    continue
+                if replica is not None and replica != replica_idxs[no]:
+                    continue
+
+                snap_idxs = storage.variables['trajectory_snapshot_idx'][traj_idx[no]]
+
+                output.append(len(snap_idxs))
 
         return output
