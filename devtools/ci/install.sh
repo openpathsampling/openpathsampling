541d7433bc758db36a217aacf698ebeb27b09537
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2015-03-13 00:54:33 +0100

    anaconda MD5 uses https://

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 ### Install Miniconda
 
 MINICONDA=Miniconda-latest-Linux-x86_64.sh
-MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
+MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 wget https://repo.continuum.io/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
