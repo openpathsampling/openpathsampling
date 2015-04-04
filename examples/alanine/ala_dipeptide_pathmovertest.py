b503bbbf74c7ba476982800745842ffe2051f715
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2015-04-04 16:40:50 +0200

    fixed merge issues

--- examples/alanine/ala_dipeptide_pathmovertest.py
+++ examples/alanine/ala_dipeptide_pathmovertest.py
@@ -28,7 +28,7 @@ from openpathsampling.pathmover import PathMoverFactory as mf
 from openpathsampling.ensemble import EnsembleFactory as ef
 from openpathsampling.ensemble import (LengthEnsemble, SequentialEnsemble, AllOutXEnsemble,
                               AllInXEnsemble)
-from openpathsampling.calculation import Bootstrapping
+from openpathsampling.pathsimulator import Bootstrapping
 from openpathsampling.pathmover import PathMover, MoveDetails, SequentialMover, \
     ConditionalSequentialMover, PartialAcceptanceSequentialMover, \
     ForwardShootMover, CollapseMove
