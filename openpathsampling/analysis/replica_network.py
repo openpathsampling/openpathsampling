30a6ec8b6c0693e2768204173c548fcbbb375b03
David W.H. Swenson <dwhs@hyperblazer.net>
2015-05-07 19:37:45 +0200

    Adding skeleton of replica network

---
+++ openpathsampling/analysis/replica_network.py
@@ -0,0 +1,22 @@
+import networkx as nx
+
+class ReplicaNetwork(object):
+
+    def __init__(self, replicas):
+        pass
+
+    def analyze(self, storage):
+        pass
+
+    def diagram(self, storage=None):
+        pass
+
+    def flow(self, bottom, top):
+        pass
+
+
+def trace_interfaces_for_replica(replica, storage):
+    pass
+
+def trace_replicas_for_interface(interface, storage):
+    pass
