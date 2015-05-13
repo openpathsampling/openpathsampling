eda99df8a6406578d5fd6b1d29809717d7a7aad0
David W.H. Swenson <dwhs@hyperblazer.net>
2015-05-13 15:10:54 +0200

    Skeleton for tests of summ_traj_vols

--- openpathsampling/tests/testtransitions.py
+++ openpathsampling/tests/testtransitions.py
@@ -1,6 +1,7 @@
 from nose.tools import assert_equal, assert_not_equal, raises
 from nose.plugins.skip import SkipTest
-from test_helpers import CallIdentity, prepend_exception_message
+from test_helpers import CallIdentity, prepend_exception_message, make_1d_traj
+
 
 import openpathsampling as paths
 from openpathsampling.ensemble import *
@@ -17,5 +18,27 @@ class testTISTransition(object):
         pass
 
     def test_ensemble_statistics(self):
+        pass
+
+class testSummarizeTrajectoryVolumes(object):
+    def setup(self):
+        op = paths.CV_Function("Id", lambda snap : snap.coordinates[0][0])
+        vol1 = paths.LambdaVolume(op, 0.1, 0.5)
+        vol2 = paths.LambdaVolume(op, -0.1, 0.7)
+        vol3 = paths.LambdaVolume(op, 2.0, 2.5)
+
+        self.stateA = vol1
+        self.interstitial = vol2 & ~vol1
+        self.outInterface = ~vol2 & ~vol3
+        self.stateB = vol3
+
+
+    def _make_traj(self, string):
+        pretraj = []
+        for l in string:
+            pretraj.append({"a" : 0.3, "b" : 2.2, "i" : 0.6, "x" : 1.0}[l])
+        return make_1d_traj(coordinates=pretraj, velocities=[1.0]*len(pretraj))
+
+    def test_summary_trajectory_volumes(self):
         pass
 
