a46ca9f04527aec6551a6c4fa54a2bae7d76ee83
David W.H. Swenson <dwhs@hyperblazer.net>
2015-06-27 13:43:09 +0200

    Removed some unneeded tests; re-ordered.

--- devtools/ci/ipythontests.sh
+++ devtools/ci/ipythontests.sh
@@ -3,8 +3,6 @@
 
 cd examples/ipython
 testfail=0
-python ipynbtest.py "alanine.ipynb" || testfail=1
-python ipynbtest.py "sliced_sequential_ensembles.ipynb" || testfail=1
 python ipynbtest.py "toy_bootstrapping.ipynb" || testfail=1
 python ipynbtest.py "toy_tis.ipynb" || testfail=1
 python ipynbtest.py "toy_analysis.ipynb" || testfail=1
@@ -12,8 +10,8 @@ python ipynbtest.py "repex_networks.ipynb" || testfail=1
 python ipynbtest.py "multistate_system_setup.ipynb" || testfail=1
 python ipynbtest.py "mstis_analysis.ipynb" || testfail=1
 python ipynbtest.py "mistis_setup.ipynb" || testfail=1
-python ipynbtest.py "langevin_integrator_check.ipynb" || testfail=1
 python ipynbtest.py "sliced_sequential_ensembles.ipynb" || testfail=1
+python ipynbtest.py "alanine.ipynb" || testfail=1
 # needs to run after alanine since it need the trajectory.nc file
 python ipynbtest.py "storage_tutorial.ipynb" || testfail=1
 # python ipynbtest.py "visualization.ipynb" || testfail=1
