ae79b7ea3320e0b3e99d2589c21e5d1da35e4f41
David W.H. Swenson <dwhs@hyperblazer.net>
2015-07-03 16:26:35 +0200

    Live vis basically works!
    Still some issues around its usage in multistate_system_setup, but otherwise
    pretty good.

--- openpathsampling/live_visualization.py
+++ openpathsampling/live_visualization.py
@@ -4,34 +4,69 @@ import matplotlib.pyplot as plt
 import openpathsampling as paths
 
 class LiveVisualization(object):
-    def __init__(self, cv_x, cv_y, range_x, range_y, output_directory=None):
+    def __init__(self, network, cv_x, cv_y, xlim, ylim, output_directory=None):
+        self.network = network
         self.cv_x = cv_x
         self.cv_y = cv_y
-        self.range_x = range_x
-        self.range_y = range_y
+        self.xlim = xlim
+        self.ylim = ylim
         self.output_directory = output_directory
-        self.extent = [range_x[0], range_x[1], range_y[0], range_y[1]]
-        self.xlim = [range_x[0], range_x[1]]
-        self.ylim = [range_y[0], range_y[1]]
         self.background = None
+        self._save_bg_axes = None
 
+        self.extent = self.xlim + self.ylim
+
+        self.add_lines = [] # keeps state
+
+        default_colors = {0 : 'r', 1 : 'b', 2 : 'g', 3 : 'y', 4 : 'm', 5 : 'c'}
+        self.ensemble_colors = {}
+        for transition in self.network.sampling_transitions:
+            for i in range(len(transition.ensembles)):
+                ens = transition.ensembles[i]
+                self.ensemble_colors[ens] = default_colors[i % 6]
+        for special_type in self.network.special_ensembles:
+            for ens in self.network.special_ensembles[special_type].keys():
+                self.ensemble_colors[ens] = 'k'
+            
 
     def draw(self, mcstep):
+        self.fig, self.ax = plt.subplots()
         if self.background is not None:
-            self.fig = (self.background)
-            ax = self.fig.axes[0]
-        else:
-            self.fig, ax = plt.subplots()
-        ax.set_xlim(self.xlim)
-        ax.set_ylim(self.ylim)
+            if self._save_bg_axes is None:
+                self._save_bg_axes = self.background.axes
+            self.fig = self.background
+            for ax in self.fig.axes:
+                self.fig.delaxes(ax)
+            for ax in self._save_bg_axes:
+                self.fig.add_axes(ax)
+            self.ax = self.fig.axes[0].twinx()
+            self.ax.cla()
+            #ax = self.fig.axes[0]
+        #else:
+            #self.fig, ax = plt.subplots()
+        self.ax.set_xlim(self.xlim)
+        self.ax.set_ylim(self.ylim)
         # draw the background
         # draw the active trajectories
         for sample in mcstep.active:
-            ax.plot(self.cv_x(sample.trajectory), 
-                    self.cv_y(sample.trajectory), linewidth=2, zorder=2);
+            #lines.append(matplotlib.lines.lines2D(
+            self.ax.plot(
+                self.cv_x(sample.trajectory), 
+                self.cv_y(sample.trajectory), 
+                linewidth=2, zorder=2,
+                color=self.ensemble_colors[sample.ensemble]
+            )#)
             # draw arrowheads at the end of each active
 
         # draw the trials
+        for trial in mcstep.change.trials:
+            #lines.append(matplotlib.lines.lines2D(
+            self.ax.plot(
+                self.cv_x(trial.trajectory), 
+                self.cv_y(trial.trajectory), 
+                linewidth=1.0, zorder=3,
+                color=self.ensemble_colors[trial.ensemble]
+            )#)
             # draw arrowheads at the end of each trial
         # decorate by trial type
 
