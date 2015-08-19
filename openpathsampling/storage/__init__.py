bf81b159c51d8c57eea7bd6ab96bbcf3180e7044
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2015-08-19 16:01:23 +0200

    fix

--- openpathsampling/storage/__init__.py
+++ openpathsampling/storage/__init__.py
@@ -1,5 +1,5 @@
 from storage import Storage, StorableObjectJSON, AnalysisStorage
-from object_storage import ObjectStore, Query
+from object_storage import ObjectStore
 from snapshot_store import SnapshotStore, ConfigurationStore, MomentumStore
 from trajectory_store import TrajectoryStore
 from sample_store import SampleStore, SampleSetStore
