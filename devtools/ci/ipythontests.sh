d7625c6442c08730a2087dafba34427ef29480ac
David W.H. Swenson <dwhs@hyperblazer.net>
2015-10-09 13:28:12 -0400

    Tests for Travis

--- devtools/ci/ipythontests.sh
+++ devtools/ci/ipythontests.sh
@@ -3,6 +3,7 @@
 
 cd examples/ipython
 testfail=0
+python ipynbtest.py "stupid_test.ipynb" || testfail=1
 python ipynbtest.py "alanine.ipynb" || testfail=1
 python ipynbtest.py "sliced_sequential_ensembles.ipynb" || testfail=1
 python ipynbtest.py "toy_bootstrapping.ipynb" || testfail=1
