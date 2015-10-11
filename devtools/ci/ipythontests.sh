9e7377bcb45917e6780fa8f73b2b68c0f67db62e
David W.H. Swenson <dwhs@hyperblazer.net>
2015-10-11 14:25:48 -0400

    Turn the tests back on

--- devtools/ci/ipythontests.sh
+++ devtools/ci/ipythontests.sh
@@ -3,23 +3,23 @@
 
 cd examples/ipython
 testfail=0
-python stupid_test.py || testfail=1
-python ipynbtest.py --verbose "stupid_test.ipynb" || testfail=1
-#python ipynbtest.py "alanine.ipynb" || testfail=1
-#python ipynbtest.py "sliced_sequential_ensembles.ipynb" || testfail=1
-#python ipynbtest.py "toy_bootstrapping.ipynb" || testfail=1
-#python ipynbtest.py "toy_tis.ipynb" || testfail=1
-#python ipynbtest.py "toy_analysis.ipynb" || testfail=1
-#python ipynbtest.py "repex_networks.ipynb" || testfail=1
-#python ipynbtest.py "multistate_system_setup.ipynb" || testfail=1
-#python ipynbtest.py "mstis_analysis.ipynb" || testfail=1
-#python ipynbtest.py "mistis_setup.ipynb" || testfail=1
-#python ipynbtest.py "mistis_analysis.ipynb" || testfail=1
-#python ipynbtest.py "langevin_integrator_check.ipynb" || testfail=1
-#python ipynbtest.py "sliced_sequential_ensembles.ipynb" || testfail=1
-## needs to run after alanine since it need the trajectory.nc file
-#python ipynbtest.py "storage_tutorial.ipynb" || testfail=1
-## python ipynbtest.py "visualization.ipynb" || testfail=1
+#python stupid_test.py || testfail=1
+#python ipynbtest.py --verbose "stupid_test.ipynb" || testfail=1
+python ipynbtest.py "alanine.ipynb" || testfail=1
+python ipynbtest.py "sliced_sequential_ensembles.ipynb" || testfail=1
+python ipynbtest.py "toy_bootstrapping.ipynb" || testfail=1
+python ipynbtest.py "toy_tis.ipynb" || testfail=1
+python ipynbtest.py "toy_analysis.ipynb" || testfail=1
+python ipynbtest.py "repex_networks.ipynb" || testfail=1
+python ipynbtest.py "multistate_system_setup.ipynb" || testfail=1
+python ipynbtest.py "mstis_analysis.ipynb" || testfail=1
+python ipynbtest.py "mistis_setup.ipynb" || testfail=1
+python ipynbtest.py "mistis_analysis.ipynb" || testfail=1
+python ipynbtest.py "langevin_integrator_check.ipynb" || testfail=1
+python ipynbtest.py "sliced_sequential_ensembles.ipynb" || testfail=1
+# needs to run after alanine since it need the trajectory.nc file
+python ipynbtest.py "storage_tutorial.ipynb" || testfail=1
+# python ipynbtest.py "visualization.ipynb" || testfail=1
 cd ../..
 if [ $testfail -eq 1 ]
 then
