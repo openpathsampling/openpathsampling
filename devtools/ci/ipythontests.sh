63380b0668dbf4d2ad49629b05f7e7e05d319565
David W.H. Swenson <dwhs@hyperblazer.net>
2015-10-12 15:50:36 -0400

    Remove the test of Travis stuff

--- devtools/ci/ipythontests.sh
+++ devtools/ci/ipythontests.sh
@@ -4,7 +4,6 @@
 cd examples/ipython
 testfail=0
 #python ipynbtest.py "sliced_sequential_ensembles.ipynb" || testfail=1
-python ipynbtest.py "stupid_test.ipynb" || testfail=1
 python ipynbtest.py "toy_bootstrapping.ipynb" || testfail=1
 python ipynbtest.py "toy_tis.ipynb" || testfail=1
 python ipynbtest.py "toy_analysis.ipynb" || testfail=1
