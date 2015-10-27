087f8268a75bfd5d3af399de2f55cf604dc7af5e
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2015-10-27 14:46:28 +0100

    moved some functions to test_helpers

--- openpathsampling/tests/teststorage.py
+++ openpathsampling/tests/teststorage.py
@@ -5,74 +5,27 @@ import os
 from nose.tools import (assert_equal, assert_not_equal, assert_items_equal,
                         assert_almost_equal, raises)
 from nose.plugins.skip import Skip, SkipTest
-from test_helpers import (true_func, data_filename,
+from test_helpers import (true_func,
+                          data_filename,
                           assert_equal_array_array,
-                          assert_not_equal_array_array)
-
-import numpy.testing as npt
+                          assert_not_equal_array_array,
+                          compare_snapshot
+                          )
 
 from openpathsampling.openmm_engine import *
 from openpathsampling.snapshot import Snapshot
 from openpathsampling.storage.object_json import ObjectJSON
 
-import simtk.unit as u
-import time
-
-def assert_close_unit(v1, v2, *args, **kwargs):
-    if type(v1) is u.Quantity:
-        assert(v1.unit == v2.unit)
-        npt.assert_allclose(v1._value, v2._value, *args, **kwargs)
-    else:
-        npt.assert_allclose(v1, v2, *args, **kwargs)
-
-def compare_snapshot(snapshot1, snapshot2):
-    assert_close_unit(snapshot1.box_vectors, snapshot2.box_vectors, rtol=1e-7, atol=0)
-    assert_close_unit(snapshot1.coordinates, snapshot2.coordinates, rtol=1e-7, atol=0)
-    assert_close_unit(snapshot1.velocities, snapshot2.velocities, rtol=1e-7, atol=0)
-
-    assert_equal(snapshot1.potential_energy, snapshot2.potential_energy)
-    assert_equal(snapshot1.kinetic_energy, snapshot2.kinetic_energy)
-
-def setUp():
-    class Object():
-        pass
-    # Use the standard Alanine to generate snapshots to store for higher testing
-    global this
-
-    this = Object()
-
-    this.options = {'temperature' : 300.0 * u.kelvin,
-               'collision_rate' : 1.0 / u.picoseconds,
-               'timestep' : 1.0 * u.femtoseconds,
-               'nsteps_per_frame' : 1,
-               'n_frames_max' : 5,
-               'start_time' : time.time(),
-               'fn_initial_pdb' : data_filename("ala_small_traj.pdb"),
-               'platform' : 'fastest',
-               'solute_indices' : range(22),
-               'forcefield_solute' : 'amber96.xml',
-               'forcefield_solvent' : 'tip3p.xml'
-              }
-
-    this.template_snapshot = paths.snapshot_from_pdb(data_filename("ala_small_traj.pdb"))
-
-    # and an openmm engine
-    this.engine = paths.OpenMMEngine(options=this.options, template=this.template_snapshot)
-    this.engine.initialized = True
-
-    # run a small trajectory of a few steps that can be used to save, etc...
-    this.traj = this.engine.generate(this.template_snapshot, running=[paths.LengthEnsemble(2).can_append])
-
-    this.filename = data_filename("storage_test.nc")
-    this.filename_clone = data_filename("storage_test_clone.nc")
-
+import mdtraj as md
 
 
 class testStorage(object):
     def setUp(self):
-        # reuse objects everytime
-        for key, value in this.__dict__.iteritems():
-            setattr(self, key, value)
+        self.mdtraj = md.load(data_filename("ala_small_traj.pdb"))
+        self.traj = paths.tools.trajectory_from_mdtraj(self.mdtraj)
+
+        self.filename = data_filename("storage_test.nc")
+        self.filename_clone = data_filename("storage_test_clone.nc")
 
         self.simplifier = ObjectJSON()
         self.template_snapshot = paths.snapshot_from_pdb(data_filename("ala_small_traj.pdb"))
@@ -86,7 +39,6 @@ class testStorage(object):
             os.remove(self.filename_clone)
 
     def test_create_template(self):
-
         store = Storage(filename=self.filename, template=self.template_snapshot, mode='w')
         assert(os.path.isfile(data_filename("storage_test.nc")))
         store.close()
