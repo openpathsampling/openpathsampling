b17a123f48ec20f9ba941e3a04f90ee0e1baa229
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2015-11-01 23:34:32 +0100

    notebook updates

--- openpathsampling/tests/testpathmover.py
+++ openpathsampling/tests/testpathmover.py
@@ -138,7 +138,7 @@ class testShootingMover(object):
                                       -0.1, 0.2, 0.4, 0.6, 0.8,
                                      ])
         SampleMover.engine = self.dyn
-        op = CV_Function("myid", fcn=lambda snap :
+        op = CV_Function("myid", f=lambda snap :
                              snap.coordinates[0][0])
         stateA = CVRangeVolume(op, -100, 0.0)
         stateB = CVRangeVolume(op, 0.65, 100)
@@ -423,7 +423,7 @@ class testRandomAllowedChoiceMover(object):
                                      ])
         self.dyn.initialized = True
         SampleMover.engine = self.dyn
-        op = CV_Function("myid", fcn=lambda snap :
+        op = CV_Function("myid", f=lambda snap :
                              snap.coordinates[0][0])
         stateA = CVRangeVolume(op, -100, 0.0)
         stateB = CVRangeVolume(op, 0.65, 100)
