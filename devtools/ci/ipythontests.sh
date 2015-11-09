b4d17f1cffe886793b712f9d86028b1e4caa89a5
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2015-11-09 21:51:32 +0100

    fix merge errors

--- devtools/ci/ipythontests.sh
+++ devtools/ci/ipythontests.sh
@@ -17,9 +17,9 @@ ipynbtest.py "mistis_setup.ipynb" || testfail=1
 date
 ipynbtest.py "mistis_analysis.ipynb" || testfail=1
 date
-python ipynbtest.py --strict "test_openmm_integration.ipynb" || testfail=1
+ipynbtest.py --strict "test_openmm_integration.ipynb" || testfail=1
 date
-python ipynbtest.py "alanine.ipynb" || testfail=1
+ipynbtest.py "alanine.ipynb" || testfail=1
 
 # needs to run after alanine since it need the trajectory.nc file
 date
