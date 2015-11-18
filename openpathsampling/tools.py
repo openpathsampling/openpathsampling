4d2b0cde9ce8052ef935446d01e95132efc7f990
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2015-11-18 12:18:20 +0100

    better unit extraction

--- openpathsampling/tools.py
+++ openpathsampling/tools.py
@@ -204,24 +204,19 @@ def units_from_snapshot(snapshot):
         the simtk.unit.Unit to be used
     """
 
-    units = {}
+    units = {'length': None, 'energy': None, 'velocity': None}
+
     if snapshot.coordinates is not None:
         if hasattr(snapshot.coordinates, 'unit'):
             units['length'] = snapshot.coordinates.unit
-        else:
-            units['length'] = u.Unit({})
 
     if snapshot.potential_energy is not None:
         if hasattr(snapshot.potential_energy, 'unit'):
             units['energy'] = snapshot.potential_energy.unit
-        else:
-            units['energy'] = u.Unit({})
 
     if snapshot.velocities is not None:
         if hasattr(snapshot.velocities, 'unit'):
             units['velocity'] = snapshot.velocities.unit
-        else:
-            units['velocity'] = u.Unit({})
 
     return units
 
