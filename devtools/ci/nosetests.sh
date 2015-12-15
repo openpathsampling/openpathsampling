23d5438886ce95b6b5483c36d99c1010301e8cfa
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2015-12-15 22:37:17 +0100

    separate requirements for nosetests, ipytests and build

--- devtools/ci/nosetests.sh
+++ devtools/ci/nosetests.sh
@@ -1,5 +1,20 @@
 # This should run the nosetests in the right folder
 
+# install python pip packages needed for testing etc.
+# None of these must be necessary for build or installation
+echo travis_fold:start:install.nose.pip.packages
+echo Install pip packages
+PIP_ARGS="-U"
+$HOME/miniconda2/envs/${python}/bin/pip install $PIP_ARGS -r devtools/ci/nose-requirements-${python}.txt
+echo travis_fold:end:install.nose.pip.packages
+
+# install python conda packages needed for testing etc.
+# None of these must be necessary for build or installation
+echo travis_fold:start:install.nose.conda.packages
+echo Install pip packages
+$HOME/miniconda2/envs/${python}/bin/pip install $PIP_ARGS -r devtools/ci/nose-requirements-${python}.txt
+echo travis_fold:end:install.nose.conda.packages
+
 echo travis_fold:start:nosetests
 echo Run nose tests ...
 
