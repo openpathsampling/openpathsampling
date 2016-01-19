b8145022366e51849b697302b7cbc084bf764531
David W.H. Swenson <dwhs@hyperblazer.net>
2016-01-19 04:28:34 -0500

    nosetests --with-coverage; badge

--- devtools/ci/nosetests.sh
+++ devtools/ci/nosetests.sh
@@ -4,7 +4,7 @@ echo travis_fold:start:nosetests
 echo Run nose tests ...
 
 testfail=0
-nosetests -v -s . || testfail=1
+nosetests -v -s --with-coverage || testfail=1
 echo travis_fold:end:nosetests
 
 if [ $testfail -eq 1 ]
