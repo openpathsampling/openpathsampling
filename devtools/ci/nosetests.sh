554ab06d8a905c76235e4986dfb9233e679bb3ab
David W.H. Swenson <dwhs@hyperblazer.net>
2016-01-19 04:20:05 -0500

    Initial setup for coveralls support

--- devtools/ci/nosetests.sh
+++ devtools/ci/nosetests.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
-# This should run the nosetests in the right folder
 
 echo travis_fold:start:nosetests
 echo Run nose tests ...
 
-cd openpathsampling
-cd tests
 testfail=0
 nosetests -v -s . || testfail=1
 echo travis_fold:end:nosetests
 
-cd ../..
 if [ $testfail -eq 1 ]
 then
     exit 1
