02f86c9131fc6652b7b2edb1977e815b8efa542e
David W.H. Swenson <dwhs@hyperblazer.net>
2016-01-21 11:19:24 -0500

    Cleanup and clarifications

--- devtools/ci/git_hash.sh
+++ devtools/ci/git_hash.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-echo travis_fold:start:git_hash
+# NOTE: the pyversion is determined by ensuring we're not in the git repo
+# directory, where the local openpathsampling directory (without a
+# version.py) is the version we load
 pyversion=`python -c "import os; os.chdir(os.environ['HOME']); import openpathsampling as paths; print paths.version.full_version"`
 gitversion=`git rev-parse HEAD`
-echo travis_fold:end:git_has
-
 echo "Installed version $pyversion from git commit hash $gitversion"
