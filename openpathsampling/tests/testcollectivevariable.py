a712ea82af75e451d8c38c51adc688120e58c455
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2016-01-23 00:18:56 +0100

    revert error in test

--- openpathsampling/tests/testcollectivevariable.py
+++ openpathsampling/tests/testcollectivevariable.py
@@ -18,7 +18,7 @@ class testCV_Function(object):
 
     def setUp(self):
         self.mdtraj = md.load(data_filename("ala_small_traj.pdb"))
-        self.traj = paths.tools.trajectory_from_mdtraj(self.mdtraj, simple_topology=True)
+        self.traj = paths.tools.trajectory_from_mdtraj(self.mdtraj)
 
     def teardown(self):
         pass
