905627598fde12fc44ad5f79c425e80734b1b38e
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2016-02-18 14:50:27 +0100

    separation of openpathsampling and dynamics

--- openpathsampling/dynamics/dynamics_engine.py
+++ openpathsampling/dynamics/dynamics_engine.py
@@ -9,10 +9,10 @@ import logging
 
 import simtk.unit as u
 
-import openpathsampling as paths
 from openpathsampling.netcdfplus import StorableNamedObject
 
-import openpathsampling.dynamics as dyn
+from snapshot import AbstractSnapshot
+from trajectory import Trajectory
 
 logger = logging.getLogger(__name__)
 
@@ -51,7 +51,7 @@ class DynamicsEngine(StorableNamedObject):
         'energy' : u.Unit({})
     }
 
-    base_snapshot_type = dyn.AbstractSnapshot
+    base_snapshot_type = AbstractSnapshot
 
     def __init__(self, options=None, template=None):
         '''
@@ -183,6 +183,7 @@ class DynamicsEngine(StorableNamedObject):
         }
 
     def set_as_default(self):
+        import openpathsampling as paths
         paths.EngineMover.engine = self
 
     @property
@@ -281,7 +282,7 @@ class DynamicsEngine(StorableNamedObject):
         except:
             running = [running]
 
-        trajectory = dyn.Trajectory()
+        trajectory = Trajectory()
 
         if direction > 0:
             self.current_snapshot = snapshot
@@ -354,7 +355,7 @@ class DynamicsEngine(StorableNamedObject):
             the initial `current_snapshot`
         """
         self.start()
-        traj = dyn.Trajectory([self.generate_next_frame()
+        traj = Trajectory([self.generate_next_frame()
                                  for i in range(n_frames)])
         self.stop(traj)
         return traj
