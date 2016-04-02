6e1226720232ff144fcf42a748a4d7c928af3aa9
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2016-04-02 23:09:30 +0200

    add test for reversed snapshot bug

--- openpathsampling/tests/teststorage.py
+++ openpathsampling/tests/teststorage.py
@@ -164,4 +164,19 @@ class testStorage(object):
         store.close()
         store2.close()
 
-        pass
+    def test_reverse_bug(self):
+        store = Storage(filename=self.filename, template=self.template_snapshot, mode='w')
+        assert(os.path.isfile(self.filename))
+
+        # template is saved, but it has no reversed
+        assert(store.template._reversed is None)
+
+        rev = store.template.reversed
+
+        # save the reversed one
+        store.save(rev)
+
+        # check that the reversed one has index 1 and not 3!
+        assert(store.idx(rev) == 1)
+        store.close()
+
