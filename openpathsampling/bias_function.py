9f19b362161e6e60ffd6e0785404a1097f54b9e8
David W.H. Swenson <dwhs@hyperblazer.net>
2016-07-09 22:56:38 +0200

    Now we can add the biases together to combine them

--- openpathsampling/bias_function.py
+++ openpathsampling/bias_function.py
@@ -35,9 +35,95 @@ class BiasEnsembleTable(BiasFunction):
     # TODO: bias seems kind of fixed to Metropolis acceptance criterion --
     # is that okay elsewhere?
     def __init__(self, dataframe, ensembles_to_ids):
-        super(BiasEnsembleTable, self).__init__()
+        super(BiasEnsembleTable, self).__init__() 
         self.dataframe = dataframe
         self.ensembles_to_ids = ensembles_to_ids
+
+    def __add__(self, other):
+        # the following craziness is to get the ensembles listed in the
+        # right order: self-only, other-only, both; all preserving the order
+        # in the original (preferring self's order for things in both)
+        self_ensembles = self.ensembles_to_ids.keys()
+        other_ensembles = other.ensembles_to_ids.keys()
+        self_only = set(self_ensembles) - set(other_ensembles)
+        other_only = set(other_ensembles) - set(self_ensembles)
+        both = set(self_ensembles) & set(other_ensembles)
+        all_ensembles = [e for e in self_ensembles if e in self_only]
+        all_ensembles += [e for e in other_ensembles if e in other_only]
+        all_ensembles += [e for e in self_ensembles if e in both]
+
+        # set up the structures for initialization of the return
+        ensembles_to_ids = {ens : all_ensembles.index(ens) 
+                            for ens in all_ensembles}
+        dataframe = pd.DataFrame(index=ensembles_to_ids.values(),
+                                 columns=ensembles_to_ids.values())
+
+        # fill the dataframe
+        # to do this, we split the matrix into regions based on self_only,
+        # other_only, and both. We have to check that we don't have two
+        # versions of the 
+        for ens_from in self_only:
+            orig_from_id = self.ensembles_to_ids[ens_from]
+            for ens_to in self_only | both:
+                orig_to_id = self.ensembles_to_ids[ens_to]
+                orig_value = self.dataframe.loc[orig_from_id, orig_to_id]
+                dataframe.set_value(index=ensembles_to_ids[ens_from],
+                                    col=ensembles_to_ids[ens_to],
+                                    value=orig_value)
+
+        for ens_from in other_only:
+            orig_from_id = other.ensembles_to_ids[ens_from]
+            for ens_to in other_only | both:
+                orig_to_id = other.ensembles_to_ids[ens_to]
+                orig_value = other.dataframe.loc[orig_from_id, orig_to_id]
+                dataframe.set_value(index=ensembles_to_ids[ens_from],
+                                    col=ensembles_to_ids[ens_to],
+                                    value=orig_value)
+
+        for ens_from in both:
+            self_from_id = self.ensembles_to_ids[ens_from]
+            other_from_id = other.ensembles_to_ids[ens_from]
+            for ens_to in self_only:
+                to_id = self.ensembles_to_ids[ens_to]
+                orig_value = self.dataframe.loc[self_from_id, to_id]
+                dataframe.set_value(index=ensembles_to_ids[ens_from],
+                                    col=ensembles_to_ids[ens_to],
+                                    value=orig_value)
+            for ens_to in other_only:
+                to_id = other.ensembles_to_ids[ens_to]
+                orig_value = other.dataframe.loc[other_from_id, to_id]
+                dataframe.set_value(index=ensembles_to_ids[ens_from],
+                                    col=ensembles_to_ids[ens_to],
+                                    value=orig_value)
+            for ens_to in both:
+                self_to_id = self.ensembles_to_ids[ens_to]
+                other_to_id = self.ensembles_to_ids[ens_to]
+                try:
+                    self_value = self.dataframe.loc[self_from_id,
+                                                    self_to_id]
+                except KeyError:
+                    # expected if this is only in other
+                    self_value = None
+                try:
+                    other_value = other.dataframe.loc[other_from_id,
+                                                      other_to_id]
+                except KeyError:
+                    # expected if this is only in self
+                    other_value = None
+                if self_value is not None and other_value is not None:
+                    if self_value != other_value:
+                        msg = "Biases have differing value for same entry:"
+                        msg = " {:} != {:}".format(self_value, other_value)
+                        raise ValueError(msg)
+                value = self_value if self_value is not None else other_value
+                if value is not None:
+                    dataframe.set_value(index=ensembles_to_ids[ens_from],
+                                        col=ensembles_to_ids[ens_to],
+                                        value=value)
+                # if both self_value and other_value are None, df unchanged
+
+        return BiasEnsembleTable(dataframe, ensembles_to_ids)
+
 
     @classmethod
     def ratios_from_dictionary(cls, ratio_dictionary):
