5acf93debcea0ef9b2822542ceb52a3234d7094c
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2016-07-30 22:04:28 +0200

    more stores to subpackage

---
+++ openpathsampling/storage/stores/__init__.py
@@ -0,0 +1,8 @@
+from collectivevariable import CVStore
+from mcstep import MCStepStore
+from pathmovechange import PathMoveChangeStore
+from sample import SampleSetStore, SampleStore
+from snapshot import (
+    SnapshotWrapperStore, SnapshotValueStore, BaseSnapshotStore,
+    FeatureSnapshotStore)
+from trajectory import TrajectoryStore
