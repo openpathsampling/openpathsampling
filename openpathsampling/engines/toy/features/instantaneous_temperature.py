a2d37d512fc9172fe9a62cde151b0917cd2378c4
David W.H. Swenson <dwhs@hyperblazer.net>
2016-09-20 18:30:36 +0200

    Add initial versions of instantaneous temp/n_dofs

---
+++ openpathsampling/engines/toy/features/instantaneous_temperature.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+@property
+def n_degrees_of_freedom(snapshot):
+    topol = snapshot.engine.topology
+    return topol.n_atoms * topol.n_spatial
+
+@property
+def instantaneous_temperature(snapshot):
+    masses = snapshot.masses
+    velocities = snapshot.velocities
+    double_ke = sum([masses[i] * velocities[i].dot(velocities[i])
+                     for i in range(len(masses))])
+
+    dofs = snaphost.n_degrees_of_freedom
+
+    temperature = double_ke / dofs
+    return temperature
+
+
