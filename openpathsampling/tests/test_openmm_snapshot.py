849e180dd404da79f2ad3aa9250378295ae0b5b9
David W.H. Swenson <dwhs@hyperblazer.net>
2016-09-23 15:09:17 +0200

    test for OpenMM instantaneous temperature

--- openpathsampling/tests/test_openmm_snapshot.py
+++ openpathsampling/tests/test_openmm_snapshot.py
@@ -1,9 +1,11 @@
 import openpathsampling.engines.openmm as omm_engine
 import openpathsampling as paths
-from nose.tools import assert_equal
-from test_helpers import data_filename
+from nose.tools import assert_equal, assert_almost_equal
+from test_helpers import data_filename, assert_close_unit
 
 import openmmtools as omt
+import simtk.unit as u
+import numpy as np
 
 import logging
 
@@ -35,3 +37,21 @@ class testOpenMMSnapshot(object):
 
     def test_n_degrees_of_freedom(self):
         assert_equal(self.engine.current_snapshot.n_degrees_of_freedom, 51)
+
+    def test_instantaneous_temperature(self):
+        vel_unit = u.nanometers / u.picoseconds
+        new_velocities = [[1.0, 0.0, 0.0]] * self.n_atoms * vel_unit
+        self.engine.current_snapshot = omm_engine.Snapshot.construct(
+            coordinates=self.engine.current_snapshot.coordinates,
+            box_vectors=self.engine.current_snapshot.box_vectors,
+            velocities=new_velocities,
+            engine=self.engine
+        )
+        test_snap = self.engine.current_snapshot
+        expected_ke = sum(
+            [m * vel_unit**2 for m in test_snap.masses], 
+            0.0*u.joule
+        )
+        n_dofs = 51.0  # see test above
+        assert_close_unit(test_snap.instantaneous_temperature,
+                          expected_ke / u.BOLTZMANN_CONSTANT_kB / n_dofs)
