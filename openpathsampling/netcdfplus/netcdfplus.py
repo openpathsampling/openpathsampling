da8a5e0c3a20bef1b4c59ce90be8fff143d078c2
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2016-10-20 00:56:26 +0200

    nb update

--- openpathsampling/netcdfplus/netcdfplus.py
+++ openpathsampling/netcdfplus/netcdfplus.py
@@ -522,7 +522,7 @@ class NetCDFPlus(netCDF4.Dataset):
         for storage in self._stores.values():
             storage.restore()
             storage._created = True
-a
+
     def list_stores(self):
         """
         Return a list of registered stores
