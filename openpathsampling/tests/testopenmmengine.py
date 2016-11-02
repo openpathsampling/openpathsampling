2db3200562b4ee048584bff803af8ae1f5f877dc
David W.H. Swenson <dwhs@hyperblazer.net>
2016-11-02 21:44:30 +0100

    Complete tests (and coverage)

--- openpathsampling/tests/testopenmmengine.py
+++ openpathsampling/tests/testopenmmengine.py
@@ -21,6 +21,11 @@ from test_helpers import (
     assert_not_equal_array_array,
     raises_with_message_like)
 
+import logging
+logging.getLogger('openpathsampling.initialization').setLevel(logging.CRITICAL)
+logging.getLogger('openpathsampling.ensemble').setLevel(logging.CRITICAL)
+logging.getLogger('openpathsampling.storage').setLevel(logging.CRITICAL)
+logging.getLogger('openpathsampling.netcdfplus').setLevel(logging.CRITICAL)
 
 def setUp():
     global topology, template, system, nan_causing_template
