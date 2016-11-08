864cdbba0b091f71a7fb7951b5a1db8aca99584b
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2016-11-08 20:30:41 +0100

    update fast cache of movechange

--- openpathsampling/storage/stores/movechange.py
+++ openpathsampling/storage/stores/movechange.py
@@ -151,13 +151,19 @@ class MoveChangeStore(ObjectStore):
                     self.storage.to_uuid_chunks(input_samples_idxs)
                 obj.samples = \
                     [self.storage.samples[UUID(idx)] for idx in samples_idxs]
-                obj.input_samples = [self.storage.samples[UUID(idx)]
-                                     for idx in input_samples_idxs]
+            if len(input_samples_idxs) > 0:
+                obj.input_samples = [
+                    self.storage.samples[UUID(idx)]
+                    for idx in input_samples_idxs]
             obj.details = self.storage.details.proxy(str(details_idx))
         else:
             if len(samples_idxs) > 0:
                 obj.samples = \
                     [self.storage.samples[int(idx)] for idx in samples_idxs]
+            if len(input_samples_idxs) > 0:
+                obj.input_samples = [
+                    self.storage.samples[int(idx)]
+                    for idx in input_samples_idxs]
             obj.details = self.storage.details.proxy(int(details_idx))
 
         return obj
