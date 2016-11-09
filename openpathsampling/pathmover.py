5478733d083b956ac5571b264fd0720b616c74a1
David W.H. Swenson <dwhs@hyperblazer.net>
2016-11-09 16:35:07 +0100

    Rough draft of forward-first 2-way shooting

--- openpathsampling/pathmover.py
+++ openpathsampling/pathmover.py
@@ -643,6 +643,28 @@ class SampleMover(PathMover):
 
 class EngineMover(SampleMover):
     """Baseclass for Movers that use an engine
+
+    Notes
+    -----
+
+    A few comments for developers working with subclasses of
+    ``EngineMover``: This class is intended to do most of the grunt work for
+    a wide range of possible engine-based needs. Remember that your
+    ``selector`` can select first or final points, e.g., to extend a move.
+    In order to help you find your way through the ``EngineMover`` code,
+    here is an overview of what various private methods do:
+
+    * ``__call__``: Creates the trial. Two steps: (1) make the trajectory;
+      (2) assemble a sample to return
+    * ``_build_sample``: assembles the final sample
+    * ``_make_forward_trajectory``/``_make_backward_trajectory``: creates
+      the actual trajectory, using :class:`.PrefixTrajectoryEnsemble` or
+      :class:`.SuffixTrajectoryEnsemble` to ensure reasonable behavior (see
+      below for further discussion)
+    * ``._run``: this is what is called by ``__call__``, and it in turn
+      calls the functions to make the trajectories (depending on the nature
+      of the mover). Frequently, this is the only thing to override (two-way
+      shooting, shifting).
     """
 
     default_engine = None
@@ -688,7 +710,8 @@ class EngineMover(SampleMover):
         shooting_index = self.selector.pick(initial_trajectory)
 
         try:
-            trial_trajectory = self._run(initial_trajectory, shooting_index)
+            trial_trajectory, run_details = self._run(initial_trajectory,
+                                                      shooting_index)
 
         except paths.engines.EngineNaNError as e:
             trial = self._build_sample(
@@ -708,6 +731,7 @@ class EngineMover(SampleMover):
                 input_sample, shooting_index, trial_trajectory)
 
         trials = [trial]
+        # TODO: update details here to include run_details
 
         return trials
 
@@ -807,7 +831,7 @@ class EngineMover(SampleMover):
         else:
             raise RuntimeError("Unknown direction: " + str(self.direction))
 
-        return trial_trajectory
+        return trial_trajectory, {}
 
 
 class ForwardShootMover(EngineMover):
@@ -2092,20 +2116,46 @@ class OneWayExtendMover(RandomChoiceMover):
         return mover
 
 
-class ForwardFirstTwoWayShootingMover(EngineMover):
-    def __init__(self, ensemble, selector, engine=None):
+
+class AbstractTwoWayShootingMover(EngineMover):
+    def __init__(self, ensemble, selector, modifier, engine=None):
         super(ForwardFirstTwoWayShootingMover, self).__init__(
             ensemble=ensemble,
             target_ensemble=ensemble,
             selector=selector,
             engine=engine
         )
+        self.modifier = modifier
 
     # required for concrete class; not really used
     @property
     def direction(self):
         return 'bidrectional'
 
+    def _make_forward_trajectory(self, trajectory, initial_snapshot):
+        fwd_ens = paths.PrefixTrajectoryEnsemble(
+            self.target_ensemble,
+            trajectory[0:shooting_index]
+        )
+        fwd_partial = self.engine.generate(initial_snapshot,
+                                           running=[fwd_ens.can_append])
+        return fwd_partial
+
+    def _make_backward_trajectory(self, trajectory, initial_snapshot):
+        # run backward
+        bkwd_ens = paths.SuffixTrajectoryEnsemble(
+            self.target_ensemble,
+            trajectory[shooting_index + 1:]
+        )
+        bkwd_partial = self.engine.generate(initial_snapshot.reversed,
+                                            running=[bkwd_ens.can_append])
+        return bkwd_partial
+
+    def _run(self, trajectory, shooting_index):
+        # to override the default implementation in EngineMover
+        raise NotImplementedError
+
+class ForwardFirstTwoWayShootingMover(AbstractTwoWayShootingMover):
     def _run(self, trajectory, shooting_index):
         """
         Takes initial trajectory and shooting point, return trial trajectory.
@@ -2117,18 +2167,17 @@ class ForwardFirstTwoWayShootingMover(EngineMover):
             sh_dir="Forward-first"
         ))
 
-        fwd_snapshot = trajectory[shooting_index]
-        fwd_trajectory = self.engine.generate(
-            fwd_snapshot, [self.target_ensemble.can_append]
-        )
+        original = trajectory[shooting_index]
+        modified = self.modifier(original)
 
-        fwd_snapshot = trajectory[shooting_index]
-        fwd_trajectory = self.engine.generate(
-            fwd_snapshot, [self.target_ensemble.can_append]
-        )
-        # make the forward trajectory
+        fwd_partial = self._make_forward_trajectory(trajectory, modified)
+        bkwd_partial = self._make_backward_trajectory(trajectory, modified)
 
-        # make the backward trajectory
+        # join the two
+        trial_trajectory = bkwd_partial.reversed + fwd_partial[1:]
+
+        details = {'modified_shooting_snapshot': modified}
+        return trial_trajectory, details
 
 
 class BackwardFirstTwoWayShootingMover(PathMover):
