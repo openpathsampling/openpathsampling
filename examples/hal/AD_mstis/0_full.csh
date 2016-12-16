da8ad636397af4ff53ebdac8507127bf54c96227
Jan-Hendrik Prinz <jan.prinz@choderalab.org>
2016-12-16 10:46:57 +0100

    joint script

---
+++ examples/hal/AD_mstis/0_full.csh
@@ -0,0 +1,51 @@
+#!/bin/tcsh
+#  Batch script for single thread GPU job.
+#
+# walltime : maximum wall clock time (hh:mm:ss)
+#PBS -l walltime=36:00:00
+#
+# memory : the amount of available memory
+#PBS -l mem=4G
+#
+# join stdout and stderr
+#PBS -j oe
+#
+# spool output immediately
+#PBS -k oe
+#
+# specify queue
+#PBS -q batch
+#
+# nodes: number of nodes
+#   ppn: number of processes per node
+#  gpus: number of gpus per node
+#  GPUs are in 'exclusive' mode by default, but 'shared' keyword sets them to shared mode.
+#PBS -l nodes=1:ppn=1:gpus=1:shared
+#
+# export all my environment variables to the job
+#PBS -V
+#
+# job name (default = name of script file)
+#PBS -N AD_mstis_0_full
+#
+# mail settings (one or more characters)
+# email is sent to local user, unless another email address is specified with PBS -M option
+# n: do not send mail
+# a: send mail if job is aborted
+# b: send mail when job begins execution
+# e: send mail when job terminates
+#PBS -m n
+#
+# filename for standard output (default = <job_name>.o<job_id>)
+# at end of job, it is in directory from which qsub was executed
+# remove extra ## from the line below if you want to name your own file
+##PBS -o myoutput
+
+# Change to working directory used for job submission
+cd $PBS_O_WORKDIR
+
+# Launch my program.
+pwd
+python /cbio/jclab/home/prinzj/projects/ops/AD_mstis/AD_mstis_1_setup.py
+python /cbio/jclab/home/prinzj/projects/ops/AD_mstis/AD_mstis_2_run.py
+python /cbio/jclab/home/prinzj/projects/ops/AD_mstis/AD_mstis_5_split.py
--- examples/hal/AD_mstis/config.py
+++ examples/hal/AD_mstis/config.py
@@ -1,27 +1,55 @@
+import os
 from os.path import join
+import glob
+import re
 
 # file paths
 
-# project_path = join(
-#     '/Users', 'jan-hendrikprinz', 'Studium', 'git',
-#     'openpathsampling', 'examples', 'resources'
-# )
-#
-# resource_path = project_path
-
 project_path = join(
-    '/cbio', 'jclab', 'home', 'prinzj', 'projects', 'ops', 'AD_mstis/')
+    '/Users', 'jan-hendrikprinz', 'Studium', 'git',
+    'openpathsampling', 'examples', 'hal', 'AD_mstis'
+)
 
-resource_path = join(project_path, 'data/')
+resource_path = project_path
 
-
+# project_path = join(
+#     '/cbio', 'jclab', 'home', 'prinzj', 'projects', 'ops', 'AD_mstis/')
+#
+# resource_path = join(project_path, 'data/')
 data_path = project_path
+
+# project setting
+
+base_name = os.getenv('OPS_PROJECT_NAME', 'ala_mstis')
+
+
+project_files = glob.glob(join(data_path, base_name + '*.nc'))
+
+r_pnum = r'\[([0-9]+)\]'
+number = 1
+for f in project_files:
+    try:
+        matches = re.findall(r_pnum, f)
+        n = int(matches[-1])
+        if n >= number:
+            number = n + 1
+    except:
+        pass
+
+number = os.getenv('OPS_PROJECT_NUMBER', number)
+
+base_storage_fnc = lambda x: 'ala_mstis[%d]' % x
+base_storage_name = base_storage_fnc(number)
+
+bfn = lambda n, part: join(data_path, '%s[%d]_%s.nc' % (base_name, n, part))
+
 
 pdb_file = join(resource_path, 'AD_initial_frame.pdb')
 
-storage_setup = join(data_path, 'ala_mstis_setup.nc')
-storage_production = join(data_path, 'ala_mstis_production.nc')
-storage_resetup = join(data_path, 'ala_mstis_setup2.nc')
+storage_setup = bfn(number, 'setup')
+storage_production = bfn(number, 'production')
+storage_resetup = bfn(number + 1, 'setup')
+storage_split = bfn(number, 'split')
 
 # platform
 
