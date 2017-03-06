180c24b6f6df3d4f2ccaa80092b082d6460a3d8d
David W.H. Swenson <dwhs@hyperblazer.net>
2017-03-06 12:15:22 +0100

    Add properties for interface sets in network
    * interface_set_ensembles (dict)
    * transition_interface_sets (dict)
    
    Both of these will, I think, be part of generalizing the new
    analysis approach and making networks easier to create and use.

--- openpathsampling/high_level/network.py
+++ openpathsampling/high_level/network.py
@@ -297,6 +297,15 @@ class TISNetwork(TransitionNetwork):
         # better with it
         pass
 
+    @property
+    def interface_set_ensembles(self):
+        return {t.interfaces: t.ensembles for t in self.sampling_transitions}
+
+    @property
+    def transition_interface_sets(self):
+        # override this in subclasses with more than one interface set
+        return {t: [t.interfaces] for t in self.transitions}
+
     def set_fluxes(self, flux_dictionary):
         """
         Parameters
