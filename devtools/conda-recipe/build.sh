a028f692e0a382c5d02891406d8aef3d88c7a1e2
Jan-Hendrik Prinz <jan.prinz@choderalab.org>
2017-03-30 12:27:24 +0200

    fix ?

--- devtools/conda-recipe/build.sh
+++ devtools/conda-recipe/build.sh
@@ -2,7 +2,7 @@
 
 # this step is done automatically by conda build
 # cp -r $RECIPE_DIR/../.. $SRC_DIR
-$PYTHON setup.py install
+$PYTHON setup.py install --single-version-externally-managed --record=/tmp/record.txt
 
 # Add more build steps here, if they are necessary.
 
