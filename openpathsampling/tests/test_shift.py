c5a1585f17ba6b640a8f9341cf4182baac49740e
Sarah McCartan <sarah.c.mccartan@gmail.com>
2017-04-04 16:09:54 +0100

    Added tests for shifting

---
+++ openpathsampling/tests/test_shift.py
@@ -0,0 +1,232 @@
+from shifting import *
+import openpathsampling.tests.test_helpers as ops_test
+from openpathsampling.tests.test_helpers import make_1d_traj
+from openpathsampling.tests.test_helpers import CalvinistDynamics
+
+from nose.plugins.skip import SkipTest
+from nose.tools import (assert_equal, assert_not_equal, assert_items_equal,
+                        raises, assert_true, assert_in, assert_not_in)
+from numpy.testing import assert_allclose
+
+from openpathsampling.collectivevariable import FunctionCV
+from openpathsampling.engines.trajectory import Trajectory
+from openpathsampling.ensemble import EnsembleFactory as ef
+from openpathsampling.ensemble import LengthEnsemble
+from openpathsampling.pathmover import *
+from openpathsampling.pathmover import IdentityPathMover
+from openpathsampling.sample import Sample, SampleSet
+from openpathsampling.shooting import UniformSelector
+from openpathsampling.volume import CVDefinedVolume
+import openpathsampling.engines.toy as toys
+import openpathsampling as paths
+
+
+#logging.getLogger('openpathsampling.pathmover').setLevel(logging.CRITICAL)
+logging.getLogger('openpathsampling.initialization').setLevel(logging.CRITICAL)
+logging.getLogger('openpathsampling.ensemble').setLevel(logging.CRITICAL)
+logging.getLogger('openpathsampling.storage').setLevel(logging.CRITICAL)
+logging.getLogger('openpathsampling.netcdfplus').setLevel(logging.CRITICAL)
+
+
+#logging.getLogger('openpathsampling.pathmover').propagate = False
+#logging.getLogger('openpathsampling.initialization').propagate = False
+import collections
+
+
+from nose.plugins.skip import Skip, SkipTest
+from openpathsampling.tests.test_helpers import (
+    true_func, assert_equal_array_array, MoverWithSignature,
+    setify_ensemble_signature, reorder_ensemble_signature
+)
+
+
+from openpathsampling.high_level.move_scheme import MoveScheme, DefaultScheme
+from openpathsampling.high_level.move_strategy import *
+from openpathsampling import VolumeFactory as vf
+
+import collections
+
+import logging
+logging.getLogger('openpathsampling.initialization').setLevel(logging.CRITICAL)
+logging.getLogger('openpathsampling.ensemble').setLevel(logging.CRITICAL)
+logging.getLogger('openpathsampling.storage').setLevel(logging.CRITICAL)
+logging.getLogger('openpathsampling.netcdfplus').setLevel(logging.CRITICAL)
+		
+
+
+
+
+
+class TestShiftingMover(object):
+	def setup(self):
+
+		
+		ensemble = paths.LengthEnsemble(5)
+		traj = ops_test.make_1d_traj([0, 1, 2, 3, 4])
+		self.traj=traj
+
+		input_sample = paths.Sample(trajectory=traj,
+			ensemble=ensemble, replica=0)
+	
+		engine = ops_test.CalvinistDynamics(
+			predestination=[ -2, -1, 0, 4, 5, 6]
+			)
+		self.engine = engine
+
+		self.pathmover = ShiftingMover(ensemble, 2, self.engine)
+
+	
+	def test_run_set(self):
+	#test the length of the set of unique trajectory values is exactly 2, corresponding to the forward
+	#and backward shifted trajectory
+
+		traj_list=[]
+		detail_list=[]		
+		for i in range(100):
+			x, d = self.pathmover._run(self.traj, 0)
+			traj_list.append(x)
+			detail_list.append(d)
+
+
+		traj_set=set(traj_list)
+		
+		#y = collections.Counter(traj_list)
+		assert_equal(len(traj_set), 2) #test the number of unique trajectory values is 2
+		#print traj_list 
+		assert_not_equal(len(traj_set), 1)
+		
+
+	
+	def test_run_traj(self):
+	#test if exact trajectory coordinates for a backward and forward shift are present in the generated
+	#trajectories (by the predetermined Calvinist Dynamics Engine)
+
+		traj_fwd = ops_test.make_1d_traj([2,3,4,5,6])
+		traj_bkwd = ops_test.make_1d_traj([-2,-1,0,1,2])
+
+		traj_list=[]
+	
+		for i in range(100):
+			x, d = self.pathmover._run(self.traj, 0)
+			traj_list.append(x.xyz.tostring())
+		traj_set=set(traj_list)
+
+		assert_in(traj_fwd.xyz.tostring(),traj_set)
+		assert_in(traj_bkwd.xyz.tostring(),traj_set)
+		
+
+		
+
+	def test_run_len_traj(self):
+	#check the length of every trajectory in the generated tractory list is equal to 5
+	#also added in test to check it matches the length of initial traj and the anticipated
+	#fwd and bkwd traj all of which are length 5, this may be redundent but means test will work
+	#if length of ensemble is changed.
+		
+		traj_fwd = ops_test.make_1d_traj([2,3,4,5,6])
+		traj_bkwd = ops_test.make_1d_traj([-2,-1,0,1,2])
+
+		traj_list=[]
+	
+		for i in range(50):
+			x, d = self.pathmover._run(self.traj, 0)
+			traj_list.append(x)
+		#traj_set=set(traj_list)
+
+		for j in traj_list:
+
+			assert_equal(len(traj_fwd.xyz), len(j.xyz))
+			assert_equal(len(traj_bkwd.xyz),len(j.xyz))
+			assert_equal(len(self.traj.xyz),len(j.xyz))
+			assert_equal(len(j.xyz),5)
+		
+
+		#print(len(traj_list[1].xyz))	
+
+	def test_run_details(self):
+	#check keys and values of the dictonary output from the _run function of the Mover
+	#keys are the name of the shift - 2 choices Forward Shift and Backward Shift
+	#values are the shift length only 1 choice the input shift length 2 
+	#also test the value of the shift length is 2 in all cases
+	
+		
+		value_list=[]
+		key_list=[]		
+		for i in range(100):
+			x, d = self.pathmover._run(self.traj, 0)
+			for keys,values in d.iteritems():
+				key_list.append(keys)
+				value_list.append(values)
+					
+		
+		key_set=set(key_list)
+		value_set=set(value_list)
+		
+		assert_equal(len(value_set), 1) #test the number of unique values is 1
+		assert_equal(len(key_set), 2)  #test the number of unique keys is 2
+		
+		#assert_in(str(key_list), str('Forward Shift'))
+		#assert_in(str(key_list), str('Backward Shift'))
+		
+		
+		
+		for j in value_list:
+			assert_equal(j, 2)
+			assert_not_equal(j, 3)
+
+			#print(type(j))
+		fwd_list=[]
+		bwd_list=[]
+
+		for k in key_list:
+			if k in "Forward Shift":
+				fwd_list.append(k)
+				
+
+			 	assert_equal(str(k),"Forward Shift")
+
+			elif k in "Backward Shift":
+				bwd_list.append(k)			
+
+				assert_equal(str(k), "Backward Shift")
+		assert_equal(len(fwd_list)+len(bwd_list), 100)
+		#print(len(bwd_list))
+
+		
+#Test the Shifting Strategy by creating Test Setup to include state definitions, interfaces and
+# a network containing the ensembles of paths.
+class ShiftStrategyTestSetup(object):
+    def setup(self):
+        cvA = paths.FunctionCV(name="xA", f=lambda s : s.xyz[0][0])
+        cvB = paths.FunctionCV(name="xB", f=lambda s : -s.xyz[0][0])
+	self.stateA = paths.CVDefinedVolume(cvA, float("-inf"), -5)
+        self.stateB = paths.CVDefinedVolume(cvB, float("-inf"), -5)
+        interfacesA = paths.VolumeInterfaceSet(cvA, float("-inf"), 
+                                               [ -3, -1])
+        interfacesB = paths.VolumeInterfaceSet(cvB, float("-inf"), 
+                                               [-3, -1])
+        self.network = paths.MSTISNetwork(
+            [(self.stateA, interfacesA),
+             (self.stateB, interfacesB)],
+              ms_outers=paths.MSOuterTISInterface.from_lambdas(
+                {interfacesA: 0.0, interfacesB: 0.0}
+		)
+             )
+
+
+class TestShiftingStrategy(ShiftStrategyTestSetup):
+    def test_make_movers(self):
+        strategy = ShiftingStrategy()
+	self.strategy=strategy
+        scheme = MoveScheme(self.network) 
+        movers = strategy.make_movers(scheme)
+        
+	assert_equal(len(movers), 4) #setup contains 4 ensembles, test to check strategy applying to all of them
+	#print(movers)
+
+        for mover in movers:
+           assert_equal(str(mover), str('Shifting')) #check Shifting Mover applied using Shifting Strategy
+	
+	
+	
+
