40e21efaadfd5de296010ff31e4cfb825ae3ac82
David W.H. Swenson <dwhs@hyperblazer.net>
2017-06-28 16:58:38 +0200

    Add import of assert_same_items

--- openpathsampling/tests/test_shooting_point_analysis.py
+++ openpathsampling/tests/test_shooting_point_analysis.py
@@ -8,7 +8,8 @@ from nose.tools import (assert_equal, assert_not_equal, raises,
                         assert_almost_equal, assert_true, assert_in)
 from nose.plugins.skip import SkipTest
 from numpy.testing import assert_array_almost_equal
-from .test_helpers import make_1d_traj, data_filename, assert_items_equal
+from .test_helpers import (make_1d_traj, data_filename, assert_items_equal,
+                           assert_same_items)
 
 import openpathsampling as paths
 import openpathsampling.engines as peng
