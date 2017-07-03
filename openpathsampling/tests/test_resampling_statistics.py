39824838350f87c53afbfb2c00292a560b5e458d
David W.H. Swenson <dwhs@hyperblazer.net>
2017-07-03 11:45:05 +0200

    Move to test_helpers.assert_items_equal

--- openpathsampling/tests/test_resampling_statistics.py
+++ openpathsampling/tests/test_resampling_statistics.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from pandas.util.testing import assert_frame_equal
-from nose.tools import (assert_equal, assert_not_equal, assert_items_equal,
+from nose.tools import (assert_equal, assert_not_equal,
                         assert_almost_equal, raises)
 from nose.plugins.skip import Skip, SkipTest
 from test_helpers import (
-    true_func, assert_equal_array_array, make_1d_traj, data_filename
+    true_func, assert_equal_array_array, make_1d_traj, data_filename,
+    assert_items_equal
 )
 
 import openpathsampling as paths
