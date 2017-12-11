22bbd9cdbdf6eb12642eac55120382b4ec53f11b
David W.H. Swenson <dwhs@hyperblazer.net>
2017-12-11 07:25:12 +0530

    Start trying to test notebooks in Py3
    Currently a kind of messy thing as we have to handle each version
    individually. Hopefully, that will change soon!

--- devtools/ci/ipythontests.sh
+++ devtools/ci/ipythontests.sh
@@ -3,5 +3,11 @@
 
 ORIG_DIR=`pwd`
 cd examples/
-./ipynbtests.sh || exit 1
+if [ "$CONDA_PY" = "2.7" ]; then
+    ./ipynbtests.sh || exit 1
+elif [ "$CONDA_PY" = "3.6" ]; then
+    ./ipynbtests3.sh || exit 1
+else
+    echo "Notebook tests skipped for CONDA_PY=$CONDA_PY"
+fi
 cd $ORIG_DIR
