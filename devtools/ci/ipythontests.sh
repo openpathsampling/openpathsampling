4438311a505cb0d163697cf36bdc816757909435
David W.H. Swenson <dwhs@hyperblazer.net>
2017-12-18 04:13:04 +0100

    Finish structural cleanups of Py3 ipynb testing
    Simplify single script for all tests

--- devtools/ci/ipythontests.sh
+++ devtools/ci/ipythontests.sh
@@ -4,15 +4,4 @@
 # TODO: completely remove this in favor of putting everything into
 # examples/ipynbtests.sh
 
-ORIG_DIR=`pwd`
-cd examples/
-if [ "$CONDA_PY" = "2.7" ]; then
-    ./ipynbtests.sh || exit 1
-elif [ "$CONDA_PY" = "3.5" ]; then
-    ./ipynbtests.sh || exit 1
-elif [ "$CONDA_PY" = "3.6" ]; then
-    ./ipynbtests.sh || exit 1
-else
-    echo "Notebook tests skipped for CONDA_PY=$CONDA_PY"
-fi
-cd $ORIG_DIR
+pushd examples/ && ./ipynbtests.sh || exit 1 && popd
