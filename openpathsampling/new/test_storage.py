c320ba4d7bd8e3908a0ce7e5657b53e84bfc20ca
David W.H. Swenson <dwhs@hyperblazer.net>
2018-01-31 20:17:10 +0100

    more tests for sql-based storage

--- openpathsampling/new/test_storage.py
+++ openpathsampling/new/test_storage.py
@@ -1,11 +1,10 @@
 from storage import *
 import pytest
 
-# NOTE: you can add other SQL backends by subclassing and changing setup
 class TestSQLStorageBackend(object):
     def setup(self):
         self._delete_tmp_files()
-        self.storage = SQLStorageBackend(":memory:")
+        self.storage = self._default_backend
         self.schema = {
             'uuid': [('uuid', 'uuid'),
                      ('table', 'int'),
@@ -19,16 +18,30 @@ class TestSQLStorageBackend(object):
     def teardown(self):
         self._delete_tmp_files()
 
+    @property
+    def _default_backend(self):
+        # NOTE: test other SQL backends by subclassing and changing this
+        return SQLStorageBackend(":memory:")
+
     @staticmethod
     def _delete_tmp_files():
         if os.path.isfile("test.sql"):
             os.remove("test.sql")
 
     def test_extract_metadata(self):
-        pass
+        pytest.skip()
 
-    def test_filename_from_backend(self):
-        pass
+    @pytest.mark.parametrize('test_input,expected', [
+        (("file.sql", "sqlite"), "sqlite:///file.sql"),
+        ((":memory:", "sqlite"), "sqlite:///:memory:")
+    ])
+    def test_filename_from_backend(self, test_input, expected):
+        filename_from_backend = self.storage.filename_from_backend
+        assert filename_from_backend(*test_input) == expected
+
+    def test_filename_from_backend_unknown(self):
+        with pytest.raises(KeyError):
+            self.storage.filename_from_backend("file.sql", "foo")
 
     def _col_names_set(self, table):
         meta = self.storage.metadata
@@ -53,14 +66,14 @@ class TestSQLStorageBackend(object):
                                                         'index'])
 
     def test_register_schema_modify_fails(self):
-        # run register_schema on self.schema again
-        pass
+        with pytest.raises(TypeError):
+            self.storage.register_schema(self.schema)
 
     def test_add_to_table(self):
-        pass
+        pytest.skip()
 
     def test_load_table_data(self):
-        pass
+        pytest.skip()
 
     def test_persistence(self):
-        pass
+        pytest.skip()
