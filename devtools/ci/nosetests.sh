7d4db5fd28f72b7d18e1b5b7e5875c7f6a8f6197
David W.H. Swenson <dwhs@hyperblazer.net>
2018-11-24 14:26:17 +0100

    Switch to pytest on Travis

--- devtools/ci/nosetests.sh
+++ devtools/ci/nosetests.sh
@@ -4,7 +4,8 @@ echo travis_fold:start:nosetests
 echo Run nose tests ...
 
 testfail=0
-nosetests -v -s --with-coverage || testfail=1
+pytest -vv --cov --cov-report xml:cov.xml || testfail=1
+#nosetests -v -s --with-coverage || testfail=1
 coveralls
 echo travis_fold:end:nosetests
 
