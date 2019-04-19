665d42a21c950f49cf8f1d98468ee040f218a45a
David W.H. Swenson <dwhs@hyperblazer.net>
2019-04-20 01:45:13 +0200

    tests for GaussianBiasSelector

--- openpathsampling/tests/test_shooting.py
+++ openpathsampling/tests/test_shooting.py
@@ -8,6 +8,8 @@ from openpathsampling.tests.test_helpers import (assert_equal_array_array,
                           make_1d_traj, assert_items_equal,
                           CalvinistDynamics
                          )
+import pytest
+
 from openpathsampling.shooting import *
 from openpathsampling.pathmover import ForwardShootMover, BackwardShootMover, SampleMover
 from openpathsampling.ensemble import LengthEnsemble
@@ -37,6 +39,39 @@ class TestUniformSelector(SelectorTest):
         assert_equal(set(collections.Counter(pick_idxs).keys()), {1, 2, 3})
 
 
+class TestGaussianBiasSelector(SelectorTest):
+    def setup(self):
+        super(TestGaussianBiasSelector, self).setup()
+        self.cv = paths.FunctionCV("Id", lambda x: x.xyz[0][0])
+        self.sel = GaussianBiasSelector(self.cv, alpha=2.0, l_0=0.25)
+        self.f = [
+            0.32465246735834974, # = exp(-2.0*(-0.5-0.25)**2)
+            0.9559974818331, # = exp(-2.0*(0.1-0.25)**2)
+            0.9950124791926823, # = exp(-2.0*(0.2-0.25)**2)
+            0.9950124791926823, # = exp(-2.0*(0.3-0.25)**2)
+            0.8824969025845955, # = exp(-2.0*(0.5-0.25)**2)
+        ]
+
+    def test_pick(self):
+        picks = [self.sel.pick(self.mytraj) for _ in range(100)]
+        pick_counter = collections.Counter(picks)
+        assert set(pick_counter.keys()) == set(range(len(self.mytraj)))
+        # final test: 99.5 should happen more than 32.4
+        assert pick_counter[2] > pick_counter[0]
+
+    @pytest.mark.parametrize('frame', [0, 1, 2, 3, 4])
+    def test_f(self, frame):
+        traj = self.mytraj
+        assert self.sel.f(traj[frame], traj) == pytest.approx(self.f[frame])
+
+    @pytest.mark.parametrize('frame', [0, 1, 2, 3, 4])
+    def test_probability(self, frame):
+        norm = sum(self.f)
+        traj = self.mytraj
+        expected = pytest.approx(self.f[frame] / norm)
+        assert self.sel.probability(traj[frame], traj) == expected
+
+
 class TestFirstFrameSelector(SelectorTest):
     def test_pick(self):
         sel = FirstFrameSelector()
@@ -60,6 +95,12 @@ class TestFirstFrameSelector(SelectorTest):
             [s.coordinates[0][0] for s in samples[0].trajectory]
         )
 
+    def test_f(self):
+        pytest.skip()
+
+    def test_probability(self):
+        pytest.skip()
+
 
 class TestFinalFrameSelector(SelectorTest):
     def test_pick(self):
@@ -82,6 +123,12 @@ class TestFinalFrameSelector(SelectorTest):
             [0.1, 0.2, 0.3, 0.4, 0.5],
             [s.coordinates[0][0] for s in samples[0].trajectory]
         )
+
+    def test_f(self):
+        pytest.skip()
+
+    def test_probability(self):
+        pytest.skip()
 
 
 class TestConstrainedSelector(SelectorTest):
