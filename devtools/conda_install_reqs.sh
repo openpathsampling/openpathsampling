46cd2bdddb3809f557e4021839aafdba62683a67
David W.H. Swenson <dwhs@hyperblazer.net>
2019-06-27 10:48:17 +0200

    make python request explicit

--- devtools/conda_install_reqs.sh
+++ devtools/conda_install_reqs.sh
@@ -4,8 +4,6 @@
 
 # TODO: these should become cmd line arguments
 ENV_NAME="openpathsampling-py$CONDA_PY"
-INCLUDE_PACKAGES="all"
-
 DEVTOOLS_DIR=`dirname "${BASH_SOURCE[0]}"`
 
 conda create -q -y --name $ENV_NAME conda future pyyaml python=$CONDA_PY
@@ -15,6 +13,8 @@ conda config --env --add pinned_packages python=$CONDA_PY
 PACKAGES=`python ${DEVTOOLS_DIR}/install_recipe_requirements.py --dry ${DEVTOOLS_DIR}/conda-recipe/meta.yaml | tr "\n" " "`
 TESTING=`cat ${DEVTOOLS_DIR}/testing_requirements.txt | tr "\n" " "`
 EXTRA=`cat ${DEVTOOLS_DIR}/optional_packages.txt | tr "\n" " "`
+PY_INSTALL="python=$CONDA_PY"
 ALL_PACKAGES="$PACKAGES $TESTING $EXTRA"
-echo $ALL_PACKAGES
-conda install -y -q -c conda-forge -c omnia $ALL_PACKAGES
+#echo $ALL_PACKAGES
+echo "conda install -y -q -c conda-forge -c omnia $PY_INSTALL $ALL_PACKAGES"
+conda install -y -q -c conda-forge -c omnia $PY_INSTALL $ALL_PACKAGES
