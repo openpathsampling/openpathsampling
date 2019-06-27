3b7af7bf7941cdc839a7905ee36369f8ffa3fbb6
David W.H. Swenson <dwhs@hyperblazer.net>
2019-06-27 12:01:00 +0200

    add support for pins

--- devtools/conda_install_reqs.sh
+++ devtools/conda_install_reqs.sh
@@ -10,11 +10,12 @@ conda create -q -y --name $ENV_NAME conda future pyyaml python=$CONDA_PY
 source activate $ENV_NAME
 
 conda config --env --add pinned_packages python=$CONDA_PY
+cp $DEVTOOLS_DIR/../pinned $CONDA_PREFIX/
 PACKAGES=`python ${DEVTOOLS_DIR}/install_recipe_requirements.py --dry ${DEVTOOLS_DIR}/conda-recipe/meta.yaml | tr "\n" " "`
 TESTING=`cat ${DEVTOOLS_DIR}/testing_requirements.txt | tr "\n" " "`
 EXTRA=`cat ${DEVTOOLS_DIR}/optional_packages.txt | tr "\n" " "`
 PY_INSTALL="python=$CONDA_PY"
+#PINS=`cat ${DEVTOOLS_DIR}/../pinned | tr "\n" " "`
 ALL_PACKAGES="$PACKAGES $TESTING $EXTRA"
-#echo $ALL_PACKAGES
-echo "conda install -y -q -c conda-forge -c omnia $PY_INSTALL $ALL_PACKAGES"
-conda install -y -q -c conda-forge -c omnia $PY_INSTALL $ALL_PACKAGES
+echo "conda install -y -q -c conda-forge -c omnia $PY_INSTALL $ALL_PACKAGES" # $PINS"
+conda install -y -q -c conda-forge -c omnia $PY_INSTALL $ALL_PACKAGES #$PINS
