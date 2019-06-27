31a4a3a5cdd6e7d872433d73dc552a83a0966fa6
David W.H. Swenson <dwhs@hyperblazer.net>
2019-06-27 12:14:28 +0200

    try to force respect of the pins

--- devtools/conda_install_reqs.sh
+++ devtools/conda_install_reqs.sh
@@ -3,19 +3,23 @@
 # REQUIRES ENV VARS: in particular, must define $CONDA_PY
 
 # TODO: these should become cmd line arguments
-ENV_NAME="openpathsampling-py$CONDA_PY"
 DEVTOOLS_DIR=`dirname "${BASH_SOURCE[0]}"`
 
-conda create -q -y --name $ENV_NAME conda future pyyaml python=$CONDA_PY
-source activate $ENV_NAME
+if [ ! -z "$OPS_ENV" ]
+then
+    conda create -q -y --name $OPS_ENV conda future pyyaml python=$CONDA_PY
+    source activate $OPS_ENV
+fi
 
-conda config --env --add pinned_packages python=$CONDA_PY
-cp $DEVTOOLS_DIR/../pinned $CONDA_PREFIX/
+# for some reason, these approaches to pinning don't always work (but conda
+# always obeys if you explicitly request a pinned version)
+#conda config --env --add pinned_packages python=$CONDA_PY
+#cp $DEVTOOLS_DIR/../pinned $CONDA_PREFIX/
 PACKAGES=`python ${DEVTOOLS_DIR}/install_recipe_requirements.py --dry ${DEVTOOLS_DIR}/conda-recipe/meta.yaml | tr "\n" " "`
 TESTING=`cat ${DEVTOOLS_DIR}/testing_requirements.txt | tr "\n" " "`
 EXTRA=`cat ${DEVTOOLS_DIR}/optional_packages.txt | tr "\n" " "`
 PY_INSTALL="python=$CONDA_PY"
-#PINS=`cat ${DEVTOOLS_DIR}/../pinned | tr "\n" " "`
+PINS=`cat ${DEVTOOLS_DIR}/../pinned | tr "\n" " "`
 ALL_PACKAGES="$PACKAGES $TESTING $EXTRA"
-echo "conda install -y -q -c conda-forge -c omnia $PY_INSTALL $ALL_PACKAGES" # $PINS"
-conda install -y -q -c conda-forge -c omnia $PY_INSTALL $ALL_PACKAGES #$PINS
+echo "conda install -y -q -c conda-forge -c omnia $PY_INSTALL $ALL_PACKAGES $PINS"
+conda install -y -q -c conda-forge -c omnia $PY_INSTALL $ALL_PACKAGES $PINS
