a027ef662984c65f926e98c79bda020b40d775e6
David W.H. Swenson <dwhs@hyperblazer.net>
2019-11-15 13:15:53 +0100

    fix minimal install

--- openpathsampling/engines/openmm/features/__init__.py
+++ openpathsampling/engines/openmm/features/__init__.py
@@ -1,4 +1,10 @@
-from openpathsampling.engines.features import *
-from openpathsampling.engines.features.shared import StaticContainer, KineticContainer
-from . import masses
-from . import instantaneous_temperature
+try:
+    import simtk.openmm
+    import simtk.openmm.app
+except ImportError:
+    pass
+else:
+    from openpathsampling.engines.features import *
+    from openpathsampling.engines.features.shared import StaticContainer, KineticContainer
+    from . import masses
+    from . import instantaneous_temperature
