480b6d3ba91775f96e5ef236be2859898f19d979
David W.H. Swenson <dwhs@hyperblazer.net>
2019-11-15 07:20:35 +0100

    Tests for BresenhamInterpolation

--- openpathsampling/tests/test_path_histogram.py
+++ openpathsampling/tests/test_path_histogram.py
@@ -114,6 +114,19 @@ class TestPathHistogramBesenhamLikeInterpolate(PathHistogramTester):
         ]
 
 
+class TestPathHistogramBesenhamInterpolate(PathHistogramTester):
+    Interpolator = BresenhamInterpolation
+    def setup(self):
+        super(TestPathHistogramBesenhamInterpolate, self).setup()
+        self.expected_bins = [
+            (0, 0),  # initial
+            (1, 1), (1, 2), (2, 3), (3, 4), (3, 5), (4, 6),  # 0->1
+            (4, 5), (4, 4), (3, 3), (3, 2),  # 1->2
+            (3, 1),  # 2->3
+            (2, 1), (1, 2), (0, 2),  # 3->4
+            (1, 3), (2, 4), (3, 5), (4, 6)  # 4->5
+        ]
+
 class TestPathHistogram(object):
     # tests of fundamental things in PathHistogram, not interpolators
     def setup(self):
