36ca37d0b7462da577b894c7cb7d73e47990f308
David W.H. Swenson <dwhs@hyperblazer.net>
2020-01-31 19:43:46 +0100

    Release 1.2

--- openpathsampling/netcdfplus/version.py
+++ openpathsampling/netcdfplus/version.py
@@ -1,5 +1,5 @@
-short_version = '1.2.0.dev0'
-version = '1.2.0.dev0'
-full_version = '1.2.0.dev0'
+short_version = '1.2.0'
+version = '1.2.0'
+full_version = '1.2.0'
 git_revision = 'alpha'
 release = False
