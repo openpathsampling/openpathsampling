347a873d10222f813cc54718ca7bf95a0915aa9f
David W.H. Swenson <dwhs@hyperblazer.net>
2021-01-30 12:58:40 +0100

    bump to autorelease 0.3.0

--- .github/workflows/autorelease-default-env.sh
+++ .github/workflows/autorelease-default-env.sh
@@ -1,4 +1,4 @@
-INSTALL_AUTORELEASE="python -m pip install autorelease==0.2.3"
+INSTALL_AUTORELEASE="python -m pip install autorelease==0.3.0"
 if [ -f autorelease-env.sh ]; then
     source autorelease-env.sh
 fi
