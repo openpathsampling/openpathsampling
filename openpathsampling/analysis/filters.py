0b8aa536bc81917eeb3f4c7849d6c0fb880a2510
David W.H. Swenson <dwhs@hyperblazer.net>
2021-06-09 11:28:52 -0400

    updates to analysis filters

--- openpathsampling/analysis/filters.py
+++ openpathsampling/analysis/filters.py
@@ -8,6 +8,15 @@ class Stages(enum.Enum):
     SAMPLE_FILTER = 3
     POSTPROCESSING = 4
 
+class FilterTypes(enum.Enum):
+    UNKNOWN = 0
+    STEP = 1
+    SAMPLE = 2
+
+def _default_repr(obj):
+    return obj.name if obj.name is not None \
+            else super(obj.__class__, obj).__repr__()
+
 class _Filter(object):
     """Abstract base class for filters.
     """
@@ -25,9 +34,14 @@ class _Filter(object):
     def __invert__(self):
         return NegationFilter(self)
 
+    def __sub__(self, other):
+        return self & ~other
+
+    def __xor__(self, other):
+        return (self & ~other) | (~self & other)
+
     def __call__(self, inputs):
         return filter(self.condition, inputs)
-
 
 
 class NegationFilter(_Filter):
@@ -60,7 +74,7 @@ class AndFilterCombination(_Filter):
         return self.filter1.condition(inp) and self.filter2.condition(inp)
 
     def __repr__(self):
-        return "(" + repr(self.filter1) + " & " repr(self.filter2) + ")"
+        return "(" + repr(self.filter1) + " & " + repr(self.filter2) + ")"
 
 
 class OrFilterCombination(_Filter):
@@ -78,8 +92,20 @@ class OrFilterCombination(_Filter):
         return self.filter1.condition(inp) or self.filter2.condition(inp)
 
     def __repr__(self):
-        return "(" + repr(self.filter1) + " | " repr(self.filter2) + ")"
+        return "(" + repr(self.filter1) + " | " + repr(self.filter2) + ")"
 
+class GenericFilter(_Filter):
+    def __init__(self, condition, name):
+        self._condition = condition
+        self.name = name
+
+    def condition(self, inp):
+        return self._condition(inp)
+
+    def __repr__(self):
+        return _default_repr(self)
+
+### OPS STEP FILTERS #######################################################
 
 class CanonicalMover(_Filter):
     STAGE = Stages.STEP_FILTER
@@ -109,38 +135,41 @@ class CanonicalMover(_Filter):
         return f"(canonical mover is {self.mover}"
 
 
-class StepFilterCondition(_Filter):
+class TrialReplica(_Filter):
     STAGE = Stages.STEP_FILTER
-    def __init__(self, condition, name):
-        self._condition = condition
-        self.name = name
+    def __init__(self, replica):
+        self.replica = replica
 
     def condition(self, step):
-        return self._condition(step)
-
-    def __repr__(self):
-        return self.name
-
-RejectedSteps = StepFilterCondition(lambda step: not step.change.accepted,
-                                    name="RejectedSteps")
-AcceptedSteps = StepFilterCondition(lambda step: step.change.accepted,
-                                    name="AcceptedSteps")
-AllSteps = StepFilterCondition(lambda step: True,
-                               name="AllSteps")
+        replicas = [s.replica for s in step.change.canonical.trials]
+        return self.replica in replicas
 
 
-def _pass_thru(items):
-    for item in items:
-        yield item
+class TrialEnsemble(_Filter):
+    STAGE = Stages.STEP_FILTER
+    def __init__(self, ensemble):
+        self.ensemble = ensemble
 
-class SampleFilterCondition(_Filter):
+    def condition(self, step):
+        ensembles = [s.ensemble for s in step.change.canonical.trials]
+        return self.ensemble in ensembles
+
+
+class StepFilter(GenericFilter):
+    STAGE = Stages.STEP_FILTER
+
+RejectedSteps = StepFilter(lambda step: not step.change.accepted,
+                           name="RejectedSteps")
+AcceptedSteps = StepFilter(lambda step: step.change.accepted,
+                           name="AcceptedSteps")
+AllSteps = StepFilter(lambda step: True,
+                      name="AllSteps")
+
+
+### SAMPLE FILTERS #########################################################
+
+class SampleFilterCondition(GenericFilter):
     STAGE = Stages.SAMPLE_FILTER
-    def __init__(self, condition, name):
-        self.condition = condition
-        self.name = name
-
-    def __repr__(self):
-        return self.name
 
 AllSamples = SampleFilterCondition(lambda sample: True, name="AllSamples")
 
@@ -167,114 +196,166 @@ class Replica(_Filter):
     def __repr__(self):
         return f"Replica({self.replica})"
 
-class PostProcess(_Filter):
-    STAGE = Stages.POSTPROCESSING
-    def __init__(self, method):
-        self.method = method
+### EXTRACTOR-FILTERS ######################################################
 
-    def condition(self, samples):
-        for item in self.method(samples):
+def _pass_thru(items):
+    return items
+    for item in items:
+        yield item
+
+NOT_EXTRACTED = object()  # flag for errors on extraction
+
+def _flatten(items):
+    for item in items:
+        if item is not NOT_EXTRACTED:
             yield item
 
-def _flatten(samples):
-    for sample in samples:
-        yield sample
-
-def _list_per_step(samples):
-    return [list(samples)]
-
-Flatten = PostProcess(_flatten)
-ListPerStep = PostProcess(_list_per_step)
+def _list_per_step(items):
+    return [list(items)]
 
 class ExtractorFilter(object):
-    def __init__(self, step_filter, extractor, sample_filter, hook=None):
+    _default_secondary = None
+    # _default_postprocessor = _pass_thru
+    def __init__(self, step_filter, extractor):
         if step_filter is None:
             step_filter = AllSteps
-        if sample_filter is None:
-            sample_filter = AllSamples
-        if hook is None:
-            hook = _pass_thru
+        # if postprocessor is None:
+            # postprocessor = _default_postprocessor
 
         self.step_filter = step_filter
         self.extractor = extractor
-        self.sample_filter = sample_filter
-        self.hook = hook
+        # self.postprocessor = postprocessor
+        self.secondary_filter = self._default_secondary
 
-    def _get_postprocess(self, flatten):
-        postprocess = {True: Flatten,
-                       False: ListPerStep,
-                       None: ListPerStep}[flatten]
-        return postprocess
+    def _get_finalizer(self, flatten):
+        if flatten is None:
+            flatten = True  # default behavior is True
+        finalizer = {True: _flatten,
+                     False: _list_per_step}[flatten]
+        return finalizer
+
+    def with_filter(self, secondary_filter):
+        if self.secondary_filter != self._default_secondary:
+            raise RuntimeError("A secondary filter has already been set.")
+        self.secondary_filter = secondary_filter
+        return self
 
     def __call__(self, steps, flatten=None):
-        raise NotImplementedError()
-
-
-class SampleFilter(ExtractorFilter):
-    def __call__(self, steps, flatten=None):
-        postprocess = self._get_postprocess(flatten)
+        finalizer = self._get_finalizer(flatten)
         for step in self.step_filter(steps):
-            samples = self.extractor(step)
-            filtered = self.sample_filter(samples)
-            processed = self.hook(filtered)
-            # TODO: self.extractor_hook here?
-            finalized = postprocess(filtered)
+            extracted = self.extractor(step)
+
+            if not self.extractor.returns_iterable:
+                extracted = [extracted]
+
+            if self.secondary_filter is not None:
+                extracted = self.secondary_filter(extracted)
+            # extracted = self.postprocessor(extracted)
+            finalized = finalizer(extracted)
             for result in finalized:
                 yield result
 
+    def __and__(self, other):
+        return _AndExtractorFilter(self, other)
+
+    def __or__(self, other):
+        return _OrExtractorFilter(self, other)
+
+    def __invert__(self):
+        return _NegationExtractorFilter(self)
+
+    def _test_compatibility(self, other):
+        value_err_str = ("Can not combine ExtractorFilters that do not "
+                         "have the same {attr}: {self_attr} != "
+                         "{other_attr}")
+        if self.extractor != other.extractor:
+            raise ValueError(value_err_str.format(
+                attr='extractor',
+                self_attr=self.extractor,
+                other_attr=other.extractor
+            ))
+        if self.secondary_filter != other.secondary_filter:
+            raise ValueError(value_err_str.format(
+                attr='secondary filter',
+                self_attr=self.secondary_filter,
+                other_attr=other.secondary_filter
+            ))
+
+class _AndExtractorFilter(ExtractorFilter):
+    def __init__(self, ef1, ef2):
+        ef1._test_compatibility(ef2)
+        step_filter = ef1.step_filter & ef2.step_filter
+        self._default_secondary = ef1._default_secondary
+        super(_AndExtractorFilter, self).__init__(step_filter,
+                                                  ef1.extractor)
+
+class _OrExtractorFilter(ExtractorFilter):
+    def __init__(self, ef1, ef2):
+        ef1._test_compatibility(ef2)
+        step_filter = ef1.step_filter | ef2.step_filter
+        self._default_secondary = ef1._default_secondary
+        super(_OrExtractorFilter, self).__init__(step_filter,
+                                                  ef1.extractor)
+
+class _NegationExtractorFilter(ExtractorFilter):
+    def __init__(self, ef):
+        self._default_secondary = ef._default_secondary
+        super(_NegationExtractorFilter, self).__init__(~ef.step_filter,
+                                                       ef.extractor)
+        self.with_filter(ef.secondary_filter)
+
+
+class SampleExtractorFilter(ExtractorFilter):
+    _default_secondary = AllSamples
+
+
+### EXTRACTORS #############################################################
 
 class Extractor(object):
-    STAGE = Stages.SAMPLE_SELECTOR
     hook = _pass_thru
-    def __init__(self, extractor, name, extract_filter):
+    def __init__(self, extractor, name=None,
+                 returns_iterable=False,
+                 extractor_filter=ExtractorFilter):
         self.extractor = extractor
         self.name = name
-        self.extract_filter = extract_filter
+        self.returns_iterable = returns_iterable
+        self.extractor_filter = extractor_filter
 
     def __call__(self, step):
         return self.extractor(step)
 
-    def __matmul__(self, other):
-        if len(other) != 2:
-            # TODO: better error
-            raise ValueError("Requires a tuple of 2 items")
-        # TODO: test that the inputs are the correct type
-        step_filter, sample_filter = other
-        return self.using(step_filter, sample_filter)
-
     def using(self, step_filter=None, sample_filter=None):
-        return self.extract_filter(step_filter=step_filter,
-                                   extractor=self,
-                                   sample_filter=sample_filter)
+        return self.extractor_filter(step_filter=step_filter,
+                                     extractor=self)
 
     def __repr__(self):
-        return self.name
+        return _default_repr(self)
+
+
+class CanonicalDetails(Extractor):
+    def __init__(self, detail_name, returns_iterable=False):
+        super(CanonicalDetails, self).__init__(
+            extractor=lambda step: getattr(step.change.canonical.details,
+                                           detail_name,
+                                           None),
+            name="CanonicalDetails({detail_name})".format(detail_name),
+            returns_iterable=returns_iterable
+        )
+
 
 class SampleExtractor(Extractor):
     def __init__(self, extractor, name):
-        super(SampleExtractor, self).__init__(extractor=extractor,
-                                              name=name,
-                                              extract_filter=SampleFilter)
-
-class ActiveEnsembles(Extractor):
-    def __init__(self, ensemble):
-        super().__init__(extractor=lambda step: step.active_ensembles,
-                         name=(f"<Ensemble named={ensemble.name} "
-                               f"uuid={ensemble.__uuid__}>"))
-        self.ensemble = ensemble
-
-    def hook(self, samples):
-        return [paths.SampleSet(samples)[self.ensemble]]
-
-
-
+        super(SampleExtractor, self).__init__(
+            extractor=extractor,
+            name=name,
+            returns_iterable=True,
+            extractor_filter=SampleExtractorFilter
+        )
 
 ActiveSamples = SampleExtractor(lambda step: step.active.samples,
                                name="ActiveSamples")
 TrialSamples = SampleExtractor(lambda step: step.change.canonical.trials,
                               name="TrialSamples")
-
-
 
 
 def _get_shooting_point(step):
@@ -285,36 +366,10 @@ def _get_shooting_point(step):
         shooting_pt = None
     return shooting_pt
 
-ShootingSteps = StepFilterCondition(
+ShootingSteps = StepFilter(
    lambda step: _get_shooting_point(step) is not None,
    name="ShootingSteps"
 )
 
-ShootingPoints = Extractor(_get_shooting_point, name="ShootingPoints",
-                           extract_filter=None)
+ShootingPoints = Extractor(_get_shooting_point, name="ShootingPoints")
 
-class _ShootingPointCondition(_Filter):
-   STAGE = Stages.SAMPLE_FILTER
-   def __init__(self, shooting_point):
-       super().__init__()
-       self.shooting_point = shooting_point
-
-   def condition(self, sample):
-       return self.shooting_point in sample.trajectory
-        
-
-# TODO: can this ExtractFilter just be generalized?
-class ShootingPointFilter(ExtractorFilter):
-   PayloadFilter = _ShootingPointCondition
-   def __call__(self, steps, flatten=None):
-       postprocess = self._get_postprocess(flatten)
-       for step in self.step_filter(steps):
-           payload = self.extractor(step)
-           samples = TrialSamples(step)
-           sample_filter = self.sample_filter & self.PayloadFilter(payload)
-           filtered = self.sample_filter(samples)
-
-
-# once we can mix stages:
-# TPSActiveSamples = ActiveSamples & Flatten
-# RejectedTrials = RejectedSteps & TrialSamples
