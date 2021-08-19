9dd11291a9a1f37bfafd196e1d290c01b2a50ecc
Bradley Dice <bdice@bradleydice.com>
2021-08-18 21:05:51 -0500

    Remove unused imports.

--- openpathsampling/engines/hoomd/__init__.py
+++ openpathsampling/engines/hoomd/__init__.py
@@ -7,19 +7,8 @@ try:
 except ImportError:
     HAS_HOOMD = False
     Engine = missing_hoomd
-    empty_snapshot_from_openmm_topology = missing_hoomd
-    snapshot_from_pdb = missing_hoomd
-    snapshot_from_testsystem = missing_hoomd
-    to_openmm_topology = missing_hoomd
-    trajectory_from_mdtraj = missing_hoomd
-    trajectory_to_mdtraj = missing_hoomd
     Snapshot = missing_hoomd
 else:
     from .engine import HOOMDEngine as Engine
-
     from . import features
-
     from .snapshot import Snapshot
-    from . import topology
-
-from openpathsampling.engines import NoEngine, SnapshotDescriptor
