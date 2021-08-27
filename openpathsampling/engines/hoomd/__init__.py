d6548dd347bd23dd3fb54332f7af6e5531a64da6
Bradley Dice <bdice@bradleydice.com>
2021-08-27 14:25:59 -0500

    Require HOOMD version >= 3.0.0.

--- openpathsampling/engines/hoomd/__init__.py
+++ openpathsampling/engines/hoomd/__init__.py
@@ -1,10 +1,12 @@
 def missing_hoomd(*args, **kwargs):
-    raise RuntimeError("Install HOOMD-blue >= 3.0 to use this feature")
+    raise RuntimeError("Install HOOMD-blue >= 3.0.0 to use this feature")
 
 
 try:
     import hoomd
-except ImportError:
+    if hoomd.version.version < "3.0.0":
+        raise ValueError("HOOMD-blue >= 3.0.0 is required.")
+except (ImportError, AttributeError, ValueError):
     HAS_HOOMD = False
     Engine = missing_hoomd
     Snapshot = missing_hoomd
