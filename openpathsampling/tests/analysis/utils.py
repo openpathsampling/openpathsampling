31c7668c0b20955ef0f604fa9af97ba87afd2024
David W.H. Swenson <dwhs@hyperblazer.net>
2021-09-12 12:17:00 -0400

    docstrings

--- openpathsampling/tests/analysis/utils.py
+++ openpathsampling/tests/analysis/utils.py
@@ -50,6 +50,20 @@ def make_trajectory(lower, upper):
     For each trajectory, some random value epsilon is added, such that the
     lowest value is ``lower + epsilon`` and the highest value is ``upper +
     epsilon``.
+
+    Parameters
+    ----------
+    lower : float
+        the lower bound x-value of the trajectory
+    upper: float
+        the upper bound x-value of the trajectory (NOTE: trajectories will
+        cross this value, but not cross ``upper + 0.1``)
+
+
+    Returns
+    -------
+    :class:`.Trajectory`
+        the trajectory segment
     """
     xvals = np.arange(lower, upper + 0.01, 0.1) + np.random.random() * 0.1
     return make_1d_traj(xvals)
@@ -66,6 +80,10 @@ def make_tis_trajectory(cv_max, lower_bound=-0.1):
         return increasing + decreasing
 
 def _select_by_input_ensembles(movers, ensembles):
+    """Select a mover by input ensembles.
+
+    Pick a random mover if ensembles is ``None``.
+    """
     # quick return if we don't actually care about which mover we use
     if ensembles is None:
         return random.choice(movers)
@@ -83,6 +101,7 @@ def _select_by_input_ensembles(movers, ensembles):
     return mover
 
 def _run_patched(mover, patches, inputs):
+    """Run the move with the given patches for the given inputs"""
     for patch in patches:
         patch.start()
 
@@ -94,6 +113,18 @@ def _run_patched(mover, patches, inputs):
 
 
 class MockRandomChoiceMover(object):
+    """Mock random choice mover to give the desired subchange.
+
+    This differs from the regular mover mocks in that it is only created
+    when we have already determined the subchange that will be generated.
+
+    Parameters
+    ----------
+    random_mover: class:`.RandomChoiceMover`
+        the mover to mock the behavior or
+    change : :class:`.MoveChange`
+        the change that the submover would return
+    """
     def __init__(self, random_mover, change):
         self.random_mover = random_mover
         self.change = change
@@ -120,12 +151,16 @@ class MockRandomChoiceMover(object):
 
 
 class MockMove(object):
+    """Abstract object for mocking a single step of a path simulation.
+    """
     def __init__(self, scheme, ensembles=None, group_name=None):
         self.scheme = scheme
         self.ensembles = ensembles
         self.group_name = group_name
 
     def wrap_org_by_group(self, change, inputs):
+        """Wraps as if using an OrganizeByMoveGroup strategy
+        """
         # extract the root_mover (selects type of move) and the
         # group_selector (selects a specific move within the move type)
         root_mover = self.scheme.root_mover
@@ -149,9 +184,35 @@ class MockMove(object):
         return outer_change
 
     def patches(self, mover):
+        """Patches to be used by this mover
+
+        Subclasses will override this when they need to control the mover's
+        behavior.
+
+        Parameters
+        ----------
+        mover : :class:`.PathMover`
+            the path mover to patch
+
+        Returns
+        -------
+        List[unittest.patch]
+            patch objects to use during the move
+        """
         return []
 
     def safety_checks(self, change, inputs):
+        """Safety checks to ensure reasonable output.
+
+        Override in subclasses.
+
+        Parameters
+        ----------
+        change : :class:`.MoveChange`
+            move change from this mocked step
+        inputs : :class:`.SampleSet`
+            original input to this mover
+        """
         pass
 
     def _do_move(self, mover, inputs):
@@ -170,6 +231,8 @@ class MockMove(object):
 
 class _MockSingleEnsembleMove(MockMove):
     def __init__(self, scheme, ensemble, group_name):
+        if ensemble is not None:
+            ensemble = [ensemble]
         super(_MockSingleEnsembleMove, self).__init__(
             scheme=scheme,
             ensembles=ensemble,
@@ -178,10 +241,16 @@ class _MockSingleEnsembleMove(MockMove):
 
     @property
     def ensemble(self):
-        return self.ensembles
+        return self.ensembles[0]
 
 
 class _MockOneWayShooting(_MockSingleEnsembleMove):
+    """Abstract mock for one-way shooting.
+
+    Core logic implemented in here (see methods ``patches`` and
+    ``safety_checks``). For usage information, see MockForwardShooting and
+    MockBackwardShooting.
+    """
     def __init__(self, shooting_index, partial_traj, direction, scheme,
                  ensemble, accepted, group_name):
         super(_MockOneWayShooting, self).__init__(scheme=scheme,
@@ -269,6 +338,31 @@ class _MockOneWayShooting(_MockSingleEnsembleMove):
 
 
 class MockForwardShooting(_MockOneWayShooting):
+    """Mock a forward shooting move.
+
+    Parameters
+    ----------
+    shooting_index : int
+        the index of the shooting point in the input trajectory
+    partial_traj : :class:`.Trajectory`
+        The trajectory to mock as coming from the engine. This is the
+        trajectory *without* the shooting point -- new frames only.
+    scheme : :class:`.MoveScheme`
+        the move scheme used in the mock simulation
+    ensemble : :class:`.Ensemble` or None
+        the ensemble to shoot from. If ``None``, a random mover is selected
+        from the movers in ``group_name``.
+    accepted : bool or None
+        if ``True`` or ``False``, force the acceptance to by true or false,
+        respectively. If ``None`` (default) use the internal logic to
+        determine acceptance. NOTE: This only bypasses the Monte Carlo
+        proposal probability calculation. Moves that are lot legally
+        possible to accept will raise an AnalysisTestSetupError if you try
+        to force them to be accepted.
+    group_name : str
+        the name of the groups to select movers from (default
+        ``'shooting'``).
+    """
     def __init__(self, shooting_index, partial_traj, scheme, ensemble=None,
                  accepted=None, group_name='shooting'):
         super(MockForwardShooting, self).__init__(
@@ -283,6 +377,32 @@ class MockForwardShooting(_MockOneWayShooting):
 
 
 class MockBackwardShooting(_MockOneWayShooting):
+    """Mock a backward shooting move.
+
+    Parameters
+    ----------
+    shooting_index : int
+        the index of the shooting point in the input trajectory
+    partial_traj : :class:`.Trajectory`
+        The trajectory to mock as coming from the engine. This is the
+        trajectory *without* the shooting point -- new frames only.
+    scheme : :class:`.MoveScheme`
+        the move scheme used in the mock simulation
+    ensemble : :class:`.Ensemble` or None
+        the ensemble to shoot from. If ``None``, a random mover is selected
+        from the movers in ``group_name``.
+    accepted : bool or None
+        if ``True`` or ``False``, force the acceptance to by true or false,
+        respectively. If ``None`` (default) use the internal logic to
+        determine acceptance. NOTE: This only bypasses the Monte Carlo
+        proposal probability calculation. Moves that are lot legally
+        possible to accept will raise an AnalysisTestSetupError if you try
+        to force them to be accepted.
+    group_name : str
+        the name of the groups to select movers from (default
+        ``'shooting'``).
+    """
+
     def __init__(self, shooting_index, partial_traj, scheme, ensemble=None,
                  accepted=None, group_name='shooting'):
         super(MockBackwardShooting, self).__init__(
@@ -297,6 +417,19 @@ class MockBackwardShooting(_MockOneWayShooting):
 
 
 class MockRepex(MockMove):
+    """Mock a replica exchange move.
+
+    Parameters
+    ----------
+    scheme : :class:`.MoveScheme`
+        the move scheme used in the mock simulation
+    ensembles : List[:class:`.Ensemble`] or None
+        The input ensembles for the replica exchange move. If ``None``, a
+        random mover is selected.
+    group_name : str
+        the name of the groups to select movers from (default
+        ``'repex'``).
+    """
     def __init__(self, scheme, ensembles=None, group_name='repex'):
         super(MockRepex, self).__init__(scheme=scheme,
                                         ensembles=ensembles,
@@ -304,6 +437,19 @@ class MockRepex(MockMove):
 
 
 class MockPathReversal(_MockSingleEnsembleMove):
+    """Mock a path reversal  move.
+
+    Parameters
+    ----------
+    scheme : :class:`.MoveScheme`
+        the move scheme used in the mock simulation
+    ensemble : List[:class:`.Ensemble`] or None
+        The input ensemble for the path reversal move. If ``None``, a
+        random mover is selected.
+    group_name : str
+        the name of the groups to select movers from (default
+        ``'pathreversal'``).
+    """
     def __init__(self, scheme, ensemble=None, group_name='pathreversal'):
         super(MockPathReversal, self).__init__(scheme=scheme,
                                                ensemble=ensemble,
@@ -317,6 +463,29 @@ def _do_single_step(init_conds, move, org_by_group):
     return change
 
 def run_moves(init_conds, moves, org_by_group=True):
+    """Run a sequence of moves.
+
+    Once the individual moves have been mocked up, this runs them to iterate
+    over the steps that you would get from the simulation.
+
+    Since this is a generator, you may want to wrap it in a list when using
+    it.
+
+    Parameters
+    ----------
+    init_conds : :class:`.SampleSet`
+        initial conditions for this run
+    moves : List[:class:`.MockMove`]
+        the moves to run, in the order they will be applied
+    org_by_group : bool
+        whether to wrap each resulting change to include the structural move
+        changes that come from the :class:`.OrganizeByMoveGroupStrategy`.
+
+    Yields
+    ------
+    :class:`.MCStep`
+        the step for each mock move
+    """
     for stepnum, move in enumerate(moves):
         change = _do_single_step(init_conds, move,
                                  org_by_group=org_by_group)
