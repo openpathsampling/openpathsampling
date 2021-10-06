623d508d7db5599308f9aa40b3c04429f198321a
sroet <sanderroet@hotmail.com>
2021-10-06 21:43:15 +0200

    add requested test without crossing

--- openpathsampling/tests/test_shooting.py
+++ openpathsampling/tests/test_shooting.py
@@ -217,7 +217,8 @@ class TestConstrainedSelector(SelectorTest):
             if (idx1 != expected_idx):
                 assert self.sel.f(frame, mytraj) == 0.0
 
-    def test_probability_ratio_modified_coordinates(self):
+    @pytest.mark.parametrize("new_coord,expected", [(0.11, 1.0), (-0.09, 0.0)])
+    def test_probability_ratio_modified_coordinates(self, new_coord, expected):
         # Test if probability ratio still works when coordinates are modified
         mytraj = make_1d_traj(coordinates=[-0.5, -0.4, -0.3, -0.1,
                                            0.1, 0.2, 0.3, 0.5])
@@ -225,9 +226,9 @@ class TestConstrainedSelector(SelectorTest):
         frame = mytraj[idx]
         # Alter 0.1 to 0.11 and replace the original snapshot with the modded
         # one
-        mod_frame = frame.copy_with_replacement(coordinates=[[0.11]])
+        mod_frame = frame.copy_with_replacement(coordinates=[[new_coord]])
         mod_traj = mytraj[:idx]
         mod_traj += paths.Trajectory([mod_frame])
-        mod_traj + mytraj[idx+1:]
+        mod_traj += mytraj[idx+1:]
         prob = self.sel.probability_ratio(frame, mytraj, mod_traj, mod_frame)
-        assert prob == 1.0
+        assert prob == expected
