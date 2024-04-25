8603f74b352061b809552c08044ba63b20a05cec
David W.H. Swenson <dwhs@hyperblazer.net>
2024-04-25 09:14:36 -0500

    devtools/ cleanup
    * Remove old scripts from Travis days
    * Remove old requirement.txt-style requirements files (setup.cfg is
      current single source of truth).

--- devtools/ci/ipythontests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Run ipython notebook tests
-
-# TODO: completely remove this in favor of putting everything into
-# examples/ipynbtests.sh
-
-pushd examples/ && ./ipynbtests.sh || exit 1 && popd
